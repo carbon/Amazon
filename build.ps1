4b57ed47b8816017f1898bfc6cf1057e95febefc
Jason Nelson <jason@carbonmade.com>
2016-11-15 11:36:55 -0600

    add solution, buildm and gitingore

---
+++ build.ps1
@@ -0,0 +1,11 @@
+dotnet pack ./src/Amazon.Core       -o ./build/Amazon.Core/
+dotnet pack ./src/Amazon.S3         -o ./build/Amazon.S3/
+dotnet pack ./src/Amazon.DynamoDb   -o ./build/Amazon.DynamoDb/
+dotnet pack ./src/Amazon.Ses        -o ./build/Amazon.Ses/
+dotnet pack ./src/Amazon.Lambda     -o ./build/Amazon.Lambda/
+dotnet pack ./src/Amazon.Kinesis    -o ./build/Amazon.Kinesis/
+dotnet pack ./src/Amazon.Sns        -o ./build/Amazon.Sns/
+dotnet pack ./src/Amazon.Kms        -o ./build/Amazon.Kms/
+dotnet pack ./src/Amazon.CloudWatch -o ./build/Amazon.CloudWatch/
+dotnet pack ./src/Amazon.Ec2        -o ./build/Amazon.Ec2/
+dotnet pack ./src/Amazon.Sqs        -o ./build/Amazon.Sqs/
