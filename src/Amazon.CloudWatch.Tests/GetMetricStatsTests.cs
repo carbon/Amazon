3e1b95903c26852a023f7379ed4928b712c52457
Jason Nelson <jason@carbonmade.com>
2016-11-29 14:38:01 -0600

    Add CloudWatch Tests project

---
+++ src/Amazon.CloudWatch.Tests/GetMetricStatsTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Amazon.CloudWatch.Tests
+{
+    public class GetMetricStatsTests
+    {
+
+     
+        [Fact]
+        public void A()
+        {
+            var text = @"
+<GetMetricStatisticsResponse xmlns=""http://monitoring.amazonaws.com/doc/2010-08-01/"">
+  <GetMetricStatisticsResult>
+    <Datapoints>
+      <member>
+        <Average>3.895104895104895</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T12:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.895104895104895</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T17:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.895104895104895</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T13:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.895104895104895</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T11:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.895104895104895</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T16:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.894345671815797</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T21:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.894342592322714</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T19:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.895104895104895</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T10:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.895104895104895</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T15:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.895104895104895</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T20:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.895104895104895</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T18:01:00Z</Timestamp>
+      </member>
+      <member>
+        <Average>3.895104895104895</Average>
+        <Unit>Count</Unit>
+        <Timestamp>2016-11-02T14:01:00Z</Timestamp>
+      </member>
+    </Datapoints>
+    <Label>HealthyHostCount</Label>
+  </GetMetricStatisticsResult>
+  <ResponseMetadata>
+    <RequestId>ef302b1f-a147-11e6-acab-872d798a60c5</RequestId>
+  </ResponseMetadata>
+</GetMetricStatisticsResponse>
+
+";
+
+
+           var result = GetMetricStatatisticsResponse.Parse(text);
+
+            Assert.Equal(3.895104895104895d, result.Datapoints[0].Average.Value);
+            Assert.Equal(12, result.Datapoints.Count);
+            Assert.Equal("HealthyHostCount", result.Label);
+
+
+            /*
+             <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>BackendConnectionErrors</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      */
+
+            var date = new DateTime(2016, 11, 02, 12, 01, 00, DateTimeKind.Utc);
+
+            Assert.Equal(date, result.Datapoints[0].Timestamp);
+            Assert.Equal(3.895104895104895, result.Datapoints[0].Average.Value);
+
+        }
+    }
+}
+ 
---
+++ src/Amazon.CloudWatch.Tests/ListMetricsResponse.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Amazon.CloudWatch.Tests
+{
+    public class ListMetricsTests
+    {
+        [Fact]
+        public void ListMetricResponse()
+        {
+            var text = @"
+<ListMetricsResponse xmlns=""http://monitoring.amazonaws.com/doc/2010-08-01/"">
+  <ListMetricsResult>
+    <Metrics>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>BackendConnectionErrors</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>HTTPCode_ELB_5XX</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>HealthyHostCount</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>HTTPCode_Backend_2XX</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>UnHealthyHostCount</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>SurgeQueueLength</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>HTTPCode_Backend_5XX</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>HTTPCode_Backend_4XX</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>HTTPCode_Backend_3XX</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>Latency</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>RequestCount</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+    </Metrics>
+  </ListMetricsResult>
+  <ResponseMetadata>
+    <RequestId>c919ffb2-a13d-11e6-994f-1d1bb7ed25dd</RequestId>
+  </ResponseMetadata>
+</ListMetricsResponse>";
+
+
+           var result = ListMetricsResponse.Parse(text);
+
+
+            Assert.Equal(11, result.Count);
+
+            /*
+             <member>
+        <Namespace>AWS/ELB</Namespace>
+        <MetricName>BackendConnectionErrors</MetricName>
+        <Dimensions>
+          <member>
+            <Name>Namespace</Name>
+            <Value>AWS</Value>
+          </member>
+        </Dimensions>
+      </member>
+      */
+
+            Assert.Equal("AWS/ELB"                 , result[0].Namespace);
+            Assert.Equal("BackendConnectionErrors" , result[0].MetricName);
+            Assert.Equal(1                         , result[0].Dimensions.Count);
+            Assert.Equal("Namespace"               , result[0].Dimensions[0].Name);
+            Assert.Equal("AWS"                     , result[0].Dimensions[0].Value);
+
+        }
+    }
+}
+ 
