b1039ce648291cef63c17c3b252c95a106de269a
Jason Nelson <jason@carbonmade.com>
2016-12-08 11:03:23 -0600

    Add a few additional services & regions

--- src/Amazon.Core/Security/CredentialScope.cs
+++ src/Amazon.Core/Security/CredentialScope.cs
@@ -6,6 +6,16 @@ namespace Amazon
     {
         public CredentialScope(DateTime date, AwsRegion region, AwsService service)
         {
+            #region Preconditions
+
+            if (region == null)
+                throw new ArgumentNullException(nameof(region));
+
+            if (service == null)
+                throw new ArgumentNullException(nameof(service));
+
+            #endregion
+
             Date = date;
             Region = region;
             Service = service;
@@ -23,7 +33,7 @@ namespace Amazon
     }
 
     // http://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
-    public class AwsService
+    public class AwsService : IEquatable<AwsService>
     {
         private AwsService(string name)
         {
@@ -32,18 +42,24 @@ namespace Amazon
 
         public string Name { get; }
 
-        public override string ToString() => Name;
+        public override string ToString() 
+            => Name;
+
+        public bool Equals(AwsService other)
+            => Name == other.Name;
 
         public static readonly AwsService Cloudfront        = new AwsService("cloudfront");
-        public static readonly AwsService Cloudwatch        = new AwsService("monitoring");
         public static readonly AwsService CloudwatchEvents  = new AwsService("events");
         public static readonly AwsService DynamoDb          = new AwsService("dynamodb");
         public static readonly AwsService Ec2               = new AwsService("ec2");
         public static readonly AwsService Elb               = new AwsService("elasticloadbalancing");
+        public static readonly AwsService ElastiCache       = new AwsService("elasticache");
+        public static readonly AwsService Glacier           = new AwsService("glacier");
         public static readonly AwsService Iam               = new AwsService("iam");
         public static readonly AwsService Kinesis           = new AwsService("kinesis");
         public static readonly AwsService Kms               = new AwsService("kms");
         public static readonly AwsService Lambda            = new AwsService("lambda");
+        public static readonly AwsService Monitoring        = new AwsService("monitoring"); // Cloudwatch monitoring
         public static readonly AwsService Ses               = new AwsService("email");
         public static readonly AwsService S3                = new AwsService("s3");
         public static readonly AwsService Sns               = new AwsService("sns");
@@ -54,7 +70,7 @@ namespace Amazon
            => new AwsService(name);
     }
 
-    public class AwsRegion
+    public class AwsRegion : IEquatable<AwsRegion>
     {
         private AwsRegion(string name)
         {
@@ -63,11 +79,17 @@ namespace Amazon
 
         public string Name { get; }
 
-        public override string ToString() => Name;
+        public override string ToString() 
+            => Name;
 
-        public static readonly AwsRegion Standard       = new AwsRegion("us-east-1");
-        public static readonly AwsRegion USEast1        = new AwsRegion("us-east-1");
+        public bool Equals(AwsRegion other)
+            => Name == other.Name;
+
+        public static readonly AwsRegion Standard       = new AwsRegion("us-east-1"); // US East (N. Virginia)
+        public static readonly AwsRegion USEast1        = Standard;
+        public static readonly AwsRegion USEast2        = new AwsRegion("us-east-2"); // US East (Ohio)
         public static readonly AwsRegion USWest1        = new AwsRegion("us-west-1");
+        public static readonly AwsRegion USWest2        = new AwsRegion("us-west-2");
         public static readonly AwsRegion SAEast1        = new AwsRegion("sa-east-1");
         public static readonly AwsRegion APNorthEast1   = new AwsRegion("ap-northeast-1");
 
