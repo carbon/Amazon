6faf44b95ac160c775a79bd46e1d1548fe28798a
Jason Nelson <jason@carbonmade.com>
2018-01-27 23:21:49 -0600

    [Ec2] Add missing properties to Image

--- src/Amazon.Ec2/Models/Image.cs
+++ src/Amazon.Ec2/Models/Image.cs
@@ -4,6 +4,10 @@ namespace Amazon.Ec2
 {
     public class Image
     {
+        // i386 | x86_64
+        [XmlElement("architecture")]
+        public string Architecture { get; set; }
+
         [XmlElement("description")]
         public string Description { get; set; }
 
@@ -19,9 +23,14 @@ namespace Amazon.Ec2
         [XmlElement("imageOwnerAlias")]
         public string ImageOwnerAlias { get; set; }
 
+        [XmlElement("imageOwnerId")]
+        public long ImageOwnerId { get; set; }
+
+        // pending | available | invalid | deregistered | transient | failed | error
         [XmlElement("imageState")]
         public string ImageState { get; set; }
 
+        // machine | kernel | ramdisk
         [XmlElement("imageType")]
         public string ImageType { get; set; }
 
@@ -34,21 +43,22 @@ namespace Amazon.Ec2
         [XmlElement("name")]
         public string Name { get; set; }
 
-        [XmlElement("architecture")]
-        public string Architecture { get; set; }
-
+        // Windows | blank
         [XmlElement("platform")]
         public string Platform { get; set; }
 
+        // ovm | xen
         [XmlElement("hypervisor")]
         public string Hypervisor { get; set; }
 
         [XmlElement("ramDiskId")]
         public string RamDiskId { get; set; }
 
+        // ebs | instance-store
         [XmlElement("rootDeviceType")]
         public string RootDeviceType { get; set; }
 
+        // hvm | paravirtual
         [XmlElement("virtualizationType")]
         public string VirtualizationType { get; set; }
     }
