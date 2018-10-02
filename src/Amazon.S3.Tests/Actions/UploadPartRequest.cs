ffe5855da77f7756f98c0dfa900aa154882e7321
Jason Nelson <jason@carbonmade.com>
2018-10-01 22:15:30 -0500

    [S3] Add UploadPartRequestTests test

---
+++ src/Amazon.S3.Tests/Actions/UploadPartRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace Amazon.S3.Models.Tests
+{
+    public class UploadPartRequestTests
+    {
+        [Fact]
+        public void Construct()
+        {
+            var a = new UploadPartRequest("s3.amazon.com", "bucket", "key", "uploadId", 1);
+
+            Assert.Equal(new Uri("https://s3.amazon.com/bucket/key?partNumber=1&uploadId=uploadId"), a.RequestUri);
+
+            Assert.Equal("uploadId", a.UploadId);
+            Assert.Equal(1, a.PartNumber);
+        }
+    }
+}
