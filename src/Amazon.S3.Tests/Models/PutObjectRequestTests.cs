4d150b6ef6ec1a5cf222145a27b71af2c819fc7f
Jason Nelson <jason@carbonmade.com>
2018-10-01 22:12:59 -0500

    [S3] Add PutObjectRequestTests test

---
+++ src/Amazon.S3.Tests/Models/PutObjectRequestTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace Amazon.S3.Tests
+{
+    public class PutObjectRequestTests
+    {
+        [Fact]
+        public void Construct()
+        {
+            var request = new PutObjectRequest("s3.amazon.com", "bucket", "key");
+
+            request.SetStorageClass(StorageClass.ReducedRedundancy);
+
+            var ms = new MemoryStream();
+
+            ms.WriteByte(1);
+
+            request.SetStream(ms, "image/jpeg");
+
+            Assert.Equal("image/jpeg", string.Join(';', request.Content.Headers.GetValues("Content-Type")));
+            Assert.Equal(new Uri("https://s3.amazon.com/bucket/key"), request.RequestUri);
+            Assert.Equal("REDUCED_REDUNDANCY", request.Headers.GetValues("x-amz-storage-class").First());
+        }
+    }
+}
