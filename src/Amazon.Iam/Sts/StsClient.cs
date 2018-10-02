6dd4b604a1238e549b9033a508e9f5516decd961
Jason Nelson <jason@carbonmade.com>
2018-10-01 22:12:32 -0500

    Seal StsClient

--- src/Amazon.Iam/Sts/StsClient.cs
+++ src/Amazon.Iam/Sts/StsClient.cs
@@ -2,12 +2,13 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 
+using Amazon.Helpers;
+
 namespace Amazon.Sts
 {
-    using Helpers;
     using Models;
 
-    public class StsClient : AwsClient
+    public sealed class StsClient : AwsClient
     {
         public const string Version = "2011-06-15";
 
@@ -17,19 +18,15 @@ namespace Amazon.Sts
 
         public async Task<AwsSession> GetSessionTokenAsync(TimeSpan duration)
         {
-            #region Preconditions
-
             // 3600s (one hour) to 129600s (36 hours), with 43200s (12 hours) as the default. 
             // Sessions for AWS account owners are restricted to a maximum of 3600s (one hour).
-
-            #endregion
 
             var request = new StsRequest("GetSessionToken") {
                 { "Version", Version },
                 { "DurationSeconds", (int)duration.TotalSeconds }
             };
 
-            var requestUri = Endpoint + request.Parameters.ToQueryString();
+            string requestUri = Endpoint + request.Parameters.ToQueryString();
 
             var httpRequest = new HttpRequestMessage(HttpMethod.Get, requestUri);
 
