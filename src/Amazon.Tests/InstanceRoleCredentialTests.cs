f4ce66710b909bb250c11cee4040b45b35e5b333
Jason Nelson <jason@carbonmade.com>
2018-10-30 11:48:43 -0500

    Add InstanceRoleCredentialTests

---
+++ src/Amazon.Tests/InstanceRoleCredentialTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Xunit;
+
+namespace Amazon.Security.Tests
+{
+    public class InstanceRoleCredentialTests
+    {
+        [Fact]
+        public void EmptyRoleNeedsRenewed()
+        {
+            var credential = new InstanceRoleCredential();
+
+            Assert.Equal(0, credential.RenewCount);
+
+            Assert.True(credential.ShouldRenew);
+        }
+
+        [Fact]
+        public void EmptyExpiresNeedsRenewed()
+        {
+            var credential = new InstanceRoleCredential("roleName");
+
+            Assert.Equal("roleName", credential.RoleName);
+            Assert.Equal(0, credential.RenewCount);
+            Assert.True(credential.Expires == default(DateTime));
+
+            Assert.True(credential.ShouldRenew);
+        }
+    }
+}
