e2379c1d3eae8de652142b9bc420bff12e04498e
Jason Nelson <jason@carbonmade.com>
2019-05-21 20:09:50 -0500

    [S3] Format GlacierJobTier

--- src/Amazon.S3/Models/GlacierJobTier.cs
+++ src/Amazon.S3/Models/GlacierJobTier.cs
@@ -1,21 +1,9 @@
 ﻿namespace Amazon.S3
 {
-    public enum GlacierJobTier
+    public enum GlacierJobTier : byte
     {
-        Standard  = 0,
-        Expedited = 1,
-        Bulk      = 2
+        Standard  = 0, // $0.01 per GB   + $0.05 per 1,000 requests  [3-5 hours]
+        Expedited = 1, // $0.03 per GP   + $10.00 per 1000 requests  [1-5 minutes]
+        Bulk      = 2  // $0.0025 per GB + $0.025 per 1,000 requests [5-12 hours]
     }
 }
-
-/*
-POST /ObjectName?restore&versionId=VersionID HTTP/1.1
-Host: BucketName.s3.amazonaws.com
-Date: date
-Authorization: authorization string (see Authenticating Requests (AWS Signature Version 4))
-Content-MD5: MD5
-
-<RestoreRequest xmlns="http://s3.amazonaws.com/doc/2006-3-01">
-   <Days>NumberOfDays</Days>
-</RestoreRequest> 
-*/
