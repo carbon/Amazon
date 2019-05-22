37dbfe9cad4bac269c8581a4b723ce6c13aa1ba1
Jason Nelson <jason@carbonmade.com>
2019-05-21 20:17:29 -0500

    [S3] Add MetadataDirective to CopyObjectRequest

--- src/Amazon.S3/Actions/CopyObjectRequest.cs
+++ src/Amazon.S3/Actions/CopyObjectRequest.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Net.Http;
 
 namespace Amazon.S3
@@ -11,5 +12,68 @@ namespace Amazon.S3
 
             CompletionOption = HttpCompletionOption.ResponseContentRead;
         }
+
+        // If undefined, the default is COPY
+
+        public MetadataDirectiveValue? MetadataDirective
+        {
+            get
+            {
+                if (this.Headers.TryGetValues("x-amz-metadata-directive", out var values))
+                {
+                    switch (values.FirstOrDefault())
+                    {
+                        case "COPY": return MetadataDirectiveValue.Copy;
+                        case "REPLACE": return MetadataDirectiveValue.Replace;
+                    }
+                }
+
+                return null;
+            }
+
+            set
+            {
+                string val = null;
+
+                switch (value)
+                {
+                    case MetadataDirectiveValue.Copy:
+                        val = "COPY";
+                        break;
+                    case MetadataDirectiveValue.Replace:
+                        val = "REPLACE";
+                        break;
+                }
+
+                Set("x-amz-metadata-directive", val);
+            }
+        }
+
+
+        private void Set(string name, string value)
+        {
+            if (value is null)
+            {
+                this.Headers.Remove(name);
+            }
+
+            this.Headers.TryAddWithoutValidation(name, value);
+        }
+
+        /*
+        x-amz-metadata-directive: metadata_directive
+        x-amz-copy-source-if-match: etag
+        x-amz-copy-source-if-none-match: etag
+        x-amz-copy-source-if-unmodified-since: time_stamp
+        x-amz-copy-source-if-modified-since: time_stamp
+        */
+
+    }
+
+
+    public enum MetadataDirectiveValue
+    {
+        Copy = 1,
+        Replace = 2
     }
 }
