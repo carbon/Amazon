264b4bf465cc051ba41d84d2a2f36c1d99c92bdb
Jason Nelson <jason@carbonmade.com>
2019-05-21 20:06:17 -0500

    Breakout KinesisStreamSubscription

--- src/Amazon.Kinesis/KinesisStream.cs
+++ src/Amazon.Kinesis/KinesisStream.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 
 using Carbon.Data.Streams;
@@ -86,53 +87,6 @@ namespace Amazon.Kinesis
                 throw new ArgumentNullException(nameof(observer));
 
             return new KinesisStreamSubscription(this, shard, observer);
-        }
-    }
-
-    public class KinesisStreamSubscription : IDisposable
-    {
-        private readonly CancellationTokenSource cancellationToken = new CancellationTokenSource();
-
-        private readonly KinesisStream stream;
-        private readonly IShard shard;
-        private readonly IObserver<IRecord> observer;
-
-        public KinesisStreamSubscription(KinesisStream stream, IShard shard, IObserver<IRecord> observer)
-        {
-            this.stream = stream;
-            this.shard = shard;
-            this.observer = observer;
-
-            Task.Factory.StartNew(async () => await StartAsync().ConfigureAwait(false));
-        }
-
-        private async Task StartAsync()
-        {
-            var iterator = await stream.GetIteratorAsync(shard, IteratorPosition.End).ConfigureAwait(false);
-
-            while (!cancellationToken.IsCancellationRequested)
-            {
-                try
-                {
-                    var result = await stream.GetAsync(iterator).ConfigureAwait(false);
-
-                    foreach (var record in result)
-                    {
-                        observer.OnNext(record);
-                    }
-
-                    iterator = result.NextIterator;
-
-                }
-                catch { }
-
-                await Task.Delay(250, cancellationToken.Token).ConfigureAwait(false);
-            }
-        }
-
-        public void Dispose()
-        {
-            cancellationToken.Cancel();
         }
     }
 }
---
+++ src/Amazon.Kinesis/KinesisStreamSubscription.cs
@@ -0,0 +1,57 @@
+﻿#nullable enable
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Carbon.Data.Streams;
+
+namespace Amazon.Kinesis
+{
+    public sealed class KinesisStreamSubscription : IDisposable
+    {
+        private readonly CancellationTokenSource cancellationToken = new CancellationTokenSource();
+
+        private readonly KinesisStream stream;
+        private readonly IShard shard;
+        private readonly IObserver<IRecord> observer;
+
+        public KinesisStreamSubscription(KinesisStream stream, IShard shard, IObserver<IRecord> observer)
+        {
+            this.stream = stream;
+            this.shard = shard;
+            this.observer = observer;
+
+            Task.Factory.StartNew(async () => await StartAsync().ConfigureAwait(false));
+        }
+
+        private async Task StartAsync()
+        {
+            var iterator = await stream.GetIteratorAsync(shard, IteratorPosition.End).ConfigureAwait(false);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var result = await stream.GetAsync(iterator).ConfigureAwait(false);
+
+                    foreach (var record in result)
+                    {
+                        observer.OnNext(record);
+                    }
+
+                    iterator = result.NextIterator;
+
+                }
+                catch { }
+
+                await Task.Delay(250, cancellationToken.Token).ConfigureAwait(false);
+            }
+        }
+
+        public void Dispose()
+        {
+            cancellationToken.Cancel();
+        }
+    }
+}
