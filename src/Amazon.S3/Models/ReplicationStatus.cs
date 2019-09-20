ed00599c6f04d5282ee4459dacb8495d65104a82
Jason Nelson <jason@carbonmade.com>
2019-09-20 13:22:11 -0500

    [S3] Add ReplicationStatus

---
+++ src/Amazon.S3/Models/ReplicationStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Amazon.S3
+{
+    public enum ReplicationStatus : byte
+    {
+        None      = 0,
+        Pending   = 1,
+        Completed = 2,
+        Failed    = 3,
+        Replica   = 4
+    }
+}
