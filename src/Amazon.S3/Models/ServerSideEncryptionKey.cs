3fbaaa740bd63834652e2fb957cede1923bf1329
Jason Nelson <jason@carbonmade.com>
2019-09-20 15:40:29 -0500

    [S3] Format ServerSideEncryptionKey

--- src/Amazon.S3/Models/ServerSideEncryptionKey.cs
+++ src/Amazon.S3/Models/ServerSideEncryptionKey.cs
@@ -7,11 +7,10 @@ namespace Amazon.S3
     {
         public ServerSideEncryptionKey(byte[] key, string algorithm = "AES256")
         {
-            if (key is null)
-                throw new ArgumentNullException(nameof(key));
-
             if (key.Length != 32)
+            {
                 throw new ArgumentException("Must be 256 bits", nameof(key));
+            }
 
             Algorithm = algorithm ?? throw new ArgumentNullException(nameof(algorithm));
             Key = key;
@@ -21,14 +20,13 @@ namespace Amazon.S3
 
         public byte[] Key { get; }
 
-        public byte[] KeyMD5
+        public readonly byte[] KeyMD5
         {
             get
             {
-                using (var md5 = MD5.Create())
-                {
-                    return md5.ComputeHash(Key);
-                }
+                using MD5 md5 = MD5.Create();
+
+                return md5.ComputeHash(Key);
             }
         }
     }
