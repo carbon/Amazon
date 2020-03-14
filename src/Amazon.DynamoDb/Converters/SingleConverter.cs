b1827f33d6025902f2a30febb34ccf4ce10b51fb
Jason Nelson <jason@carbonmade.com>
2020-03-14 17:48:40 -0500

    [CloudWatch] Crosstarget netcoreapp3.1

--- src/Amazon.DynamoDb/Converters/SingleConverter.cs
+++ src/Amazon.DynamoDb/Converters/SingleConverter.cs
@@ -6,7 +6,7 @@ namespace Amazon.DynamoDb
 {
     internal sealed class SingleConverter : IDbValueConverter
     {
-        public DbValue FromObject(object value, IMember member) => new DbValue((single)value);
+        public DbValue FromObject(object value, IMember member) => new DbValue((Single)value);
 
         public object ToObject(DbValue item, IMember member) => item.ToSingle();
     }
