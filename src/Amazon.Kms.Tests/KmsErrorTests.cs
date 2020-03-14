3e089f1c685ae90940a0210d8ecefa47944f299e
Jason Nelson <jason@carbonmade.com>
2020-03-14 18:06:08 -0500

    Add KmsErrorTests

---
+++ src/Amazon.Kms.Tests/KmsErrorTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+using Xunit;
+
+namespace Amazon.Kms.Tests
+{
+    public class KmsErrorTests
+    {
+        [Fact]
+        public void Deserialize()
+        {
+            var text = "{\"__type\":\"AccessDeniedException\",\"Message\":\"The ciphertext refers to a customer master key that does not exist, does not exist in this region, or you are not allowed to access.\"}";
+
+            var json = JsonSerializer.Deserialize<KmsError>(text);
+
+            Assert.Equal("AccessDeniedException", json.Type);
+            Assert.Equal("The ciphertext refers to a customer master key that does not exist, does not exist in this region, or you are not allowed to access.", json.Message);
+        }
+    }
+}
