59d737c9fc60f3ab04b18c57d8ea89393101ef23
Jason Nelson <jason@carbonmade.com>
2020-07-01 14:59:32 -0500

    React to Region changes

--- src/Amazon.Tests/AwsRegionTests.cs
+++ src/Amazon.Tests/AwsRegionTests.cs
@@ -9,7 +9,7 @@ namespace Amazon.Tests
         [Fact]
         public void Enum()
         {
-            Assert.Equal(18, AwsRegion.All.Length);
+            Assert.Equal(20, AwsRegion.All.Length);
 
             foreach (var region in AwsRegion.All)
             {
