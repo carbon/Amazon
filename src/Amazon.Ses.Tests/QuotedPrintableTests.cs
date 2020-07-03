2d7d7e2a629144e65921be556da85be4e023aa61
Jason Nelson <jason@carbonmade.com>
2020-07-03 16:53:57 -0500

    [S3] Breakout QuotedPrintable from SesHelper

--- src/Amazon.Ses.Tests/QuotedPrintableTests.cs
+++ src/Amazon.Ses.Tests/QuotedPrintableTests.cs
@@ -8,14 +8,25 @@ namespace Amazon.Ses.Tests
         [Fact]
         public void Decode()
         {
-            Assert.Equal("Subject", QuotedPrintable.Decode("=?utf-8?Q?Subject?="));
-            Assert.Equal("☻",       QuotedPrintable.Decode("=?UTF-8?Q?=E2=98=BB?="));
+            Assert.Equal("Subject",         QuotedPrintable.Decode("=?utf-8?Q?Subject?="));
+            Assert.Equal("☻",               QuotedPrintable.Decode("=?UTF-8?Q?=E2=98=BB?="));
+            Assert.Equal("Як ти поживаєш?", QuotedPrintable.Decode("=?utf-8?Q?=D0=AF=D0=BA_=D1=82=D0=B8_=D0=BF=D0=BE=D0=B6=D0=B8=D0=B2=D0=B0=D1=94=D1=88=3F?="));
         }
 
         [Fact]
         public void Encode()
         {   
-            Assert.Equal("=?UTF-8?Q?=E2=98=BB?=", QuotedPrintable.Encode("☻"));
+            Assert.Equal("=?utf-8?Q?=E2=98=BB?=", QuotedPrintable.Encode("☻"));
+        }
+
+        [Fact]
+        public void DeodeUtf8Code()
+        {
+            var q = "=?UTF-8?Q?=E0=B8=99=E0=B8=A0=E0=B8=B1=E0=B8=AA=E0=B8=AA=E0=B8=A3?=";
+
+            var r = QuotedPrintable.Decode(q);
+
+            Assert.Equal("นภัสสร", r);
         }
     }
 }
---
+++ src/Amazon.Ses/Helpers/QuotedPrintable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Mail;
+using System.Text;
+
+namespace Amazon.Ses
+{
+    public static class QuotedPrintable
+    {
+        public static string Encode(string text)
+        {
+            // RFC 2047.
+
+            string encoded = EncodeContent(text);
+
+            return (encoded != text)
+                ? $"=?utf-8?Q?{encoded}?="
+                : text;
+        }
+
+        public static string Decode(string text)
+        {
+            using var a = Attachment.CreateAttachmentFromString("", text);
+
+            return a.Name;
+        }
+
+        // Based on https://stackoverflow.com/questions/11793734/code-for-encode-decode-quotedprintable
+
+        private static string EncodeContent(string value)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+
+            var sb = new StringBuilder();
+
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
+
+            foreach (byte v in bytes)
+            {
+                // The following are not required to be encoded:
+                // - Tab (ASCII 9)
+                // - Space (ASCII 32)
+                // - Characters 33 to 126, except for the equal sign (61).
+
+                if ((v == 9) || ((v >= 32) && (v <= 60)) || ((v >= 62) && (v <= 126)))
+                {
+                    sb.Append(Convert.ToChar(v));
+                }
+                else
+                {
+                    sb.Append('=');
+
+                    sb.Append(v.ToString("X2"));
+                }
+            }
+
+            char lastChar = sb[sb.Length - 1];
+
+            if (char.IsWhiteSpace(lastChar))
+            {
+                sb.Remove(sb.Length - 1, 1);
+                sb.Append('=');
+                sb.Append(((int)lastChar).ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
