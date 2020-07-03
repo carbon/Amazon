ba5fa3beb59b92747aeb51e5f6e9bb27ed11cf76
Jason Nelson <jason@carbonmade.com>
2020-07-03 16:56:47 -0500

    [Route53] Improve test coverage

---
+++ src/Amazon.Route53.Tests/Actions/GetChangeResponseTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Amazon.Route53.Tests
+{
+    public class GetChangeResponseTests
+    {
+        [Fact]
+        public void Deserialize()
+        {
+            string text = @"<GetChangeResponse xmlns=""https://route53.amazonaws.com/doc/2013-04-01/"">
+   <ChangeInfo>
+      <Id>1</Id>
+      <Status>INSYNC</Status>
+      <SubmittedAt>2017-03-10T01:36:41.958Z</SubmittedAt>
+   </ChangeInfo>
+</GetChangeResponse>";
+
+            var result = Route53Serializer<GetChangeResponse>.DeserializeXml(text);
+
+            Assert.Equal("1", result.ChangeInfo.Id);
+            Assert.Equal("INSYNC", result.ChangeInfo.Status);
+            Assert.Equal(2017, result.ChangeInfo.SubmittedAt.Year);
+        }
+    }
+}
