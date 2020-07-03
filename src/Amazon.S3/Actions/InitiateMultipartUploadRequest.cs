6cf19cc4d8f3b5e2766b5c4c82e31746dedb8e75
Jason Nelson <jason@carbonmade.com>
2020-07-03 16:44:26 -0500

    [S3] Add ContentType to InitiateMultipartUploadRequest

--- src/Amazon.S3/Actions/InitiateMultipartUploadRequest.cs
+++ src/Amazon.S3/Actions/InitiateMultipartUploadRequest.cs
@@ -1,17 +1,49 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
+
+using Amazon.S3.Helpers;
 
 namespace Amazon.S3
 {
     public sealed class InitiateMultipartUploadRequest : S3Request
     {
         public InitiateMultipartUploadRequest(string host, string bucketName, string key)
-            : base(HttpMethod.Post, host, bucketName, key + "?uploads")
+            : base(HttpMethod.Post, host, bucketName, objectName: key + "?uploads")
         {
             if (key is null) throw new ArgumentNullException(nameof(key));
 
             CompletionOption = HttpCompletionOption.ResponseContentRead;
+
+            Content = new ByteArrayContent(Array.Empty<byte>());
         }
+
+        public InitiateMultipartUploadRequest(
+            string host,
+            string bucketName, 
+            string key,
+            IReadOnlyDictionary<string, string> properties)
+           : this(host, bucketName, key)
+        {
+            this.UpdateHeaders(properties);
+        }
+
+        public string? ContentType
+        {
+            get => Content.Headers.ContentType?.ToString();
+            set
+            {
+                if (value is null)
+                {
+                    Content.Headers.ContentType = null;
+                }
+                else
+                {
+                    Content.Headers.ContentType = MediaTypeHeaderValue.Parse(value);
+                }
+            }
+        }       
     }
 }
 
