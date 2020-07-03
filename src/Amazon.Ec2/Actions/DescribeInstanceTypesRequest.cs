be424056b00f97864c0fdb03701724d091a966c3
Jason Nelson <jason@carbonmade.com>
2020-07-03 16:42:52 -0500

    [Ec2] Add DescribeInstanceTypesRequest

---
+++ src/Amazon.Ec2/Actions/DescribeInstanceTypesRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Amazon.Ec2
+{
+    public sealed class DescribeInstanceTypesRequest : DescribeRequest, IEc2Request
+    {
+        public DescribeInstanceTypesRequest(params string[] instanceTypes) 
+        {
+            InstanceTypes = instanceTypes;
+        }
+
+        public string[] InstanceTypes { get; }
+
+        public Dictionary<string, string> ToParams()
+        {
+            var parameters = GetParameters("DescribeInstanceTypes");
+
+            AddIds(parameters, "InstanceType", InstanceTypes);
+
+            return parameters;
+        }
+    }
+}
