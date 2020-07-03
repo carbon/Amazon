efab257290381916909add1d0a759bd1d9530265
Jason Nelson <jason@carbonmade.com>
2020-07-03 16:45:00 -0500

    [Core] Make InstanceMetadataService public

--- src/Amazon.Core/Metadata/InstanceMetadatadataService.cs
+++ src/Amazon.Core/Metadata/InstanceMetadatadataService.cs
@@ -7,35 +7,19 @@ using System.Threading.Tasks;
 
 namespace Amazon.Metadata
 {
-    internal sealed class InstanceMetadataService
+    public sealed class InstanceMetadataService
     {
         public static readonly InstanceMetadataService Instance = new InstanceMetadataService();
+
+        private InstanceMetadataService() { }
+
+        private static readonly JsonSerializerOptions camelCasePolicy = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
         private const string baseMetadataUri = "http://169.254.169.254/latest/meta-data";
 
         private readonly HttpClient httpClient = new HttpClient {
             Timeout = TimeSpan.FromSeconds(5)
         };
-
-        private MetadataToken? token;
-
-        public async Task<MetadataToken> GetTokenAsync(TimeSpan lifetime)
-        {
-            if (lifetime < TimeSpan.Zero || lifetime > TimeSpan.FromHours(6))
-            {
-                throw new ArgumentException("Must be > 0 & less than 6 hours", nameof(lifetime));
-            }
-
-            var request = new HttpRequestMessage(HttpMethod.Put, "http://169.254.169.254/latest/api/token") {
-                Headers = { { "X-aws-ec2-metadata-token-ttl-seconds", ((int)lifetime.TotalSeconds).ToString(CultureInfo.InvariantCulture) } }
-            };
-
-            using HttpResponseMessage response = await httpClient.SendAsync(request).ConfigureAwait(false);
-
-            string responseText = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-            return new MetadataToken(responseText, DateTime.UtcNow + lifetime);
-        }
 
         // If Amazon EC2 is not preparing to stop or terminate the instance, 
         // or if you terminated the instance yourself, instance-action is 
@@ -45,12 +29,12 @@ namespace Amazon.Metadata
         {
             string? value = await GetStringOrDefaultAsync(baseMetadataUri + "/spot/instance-action").ConfigureAwait(false);
 
-            return value != null 
-                ? JsonSerializer.Deserialize<InstanceAction>(value)
-                : null;
+            if (value is null) return null;
+
+            return JsonSerializer.Deserialize<InstanceAction>(value);
         }
-      
-        public async Task<IamSecurityCredentials> GetIamSecurityCredentials(string roleName)
+
+        internal async Task<IamSecurityCredentials> GetIamSecurityCredentials(string roleName)
         {
             string requestUri = baseMetadataUri + "/iam/security-credentials/" + roleName;
 
@@ -82,8 +66,6 @@ namespace Amazon.Metadata
             throw new Exception("error getting security credentials", lastException);
         }
 
-        private static readonly JsonSerializerOptions camelCasePolicy = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-
         public async Task<InstanceIdentity> GetInstanceIdentityAsync()
         {
             using var response = await GetAsync("http://169.254.169.254/latest/dynamic/instance-identity/document").ConfigureAwait(false);
@@ -111,7 +93,7 @@ namespace Amazon.Metadata
             return GetStringAsync(baseMetadataUri + "/instance-id");
         }
 
-        public async Task<IPAddress?> GetPublicIpAsync()
+        public async Task<IPAddress?> GetPublicIpv4Async()
         {
             string? result = await GetStringOrDefaultAsync(baseMetadataUri + "/public-ipv4").ConfigureAwait(false);
 
@@ -132,7 +114,28 @@ namespace Amazon.Metadata
             return GetStringOrDefaultAsync(baseMetadataUri + "/user-data");
         }
 
-        public async ValueTask<MetadataToken> GetTokenAsync()
+        private MetadataToken? token;
+
+        private async Task<MetadataToken> GetTokenAsync(TimeSpan lifetime)
+        {
+            if (lifetime < TimeSpan.Zero || lifetime > TimeSpan.FromHours(6))
+            {
+                throw new ArgumentException("Must be > 0 & less than 6 hours", nameof(lifetime));
+            }
+
+            var request = new HttpRequestMessage(HttpMethod.Put, "http://169.254.169.254/latest/api/token")
+            {
+                Headers = { { "X-aws-ec2-metadata-token-ttl-seconds", ((int)lifetime.TotalSeconds).ToString(CultureInfo.InvariantCulture) } }
+            };
+
+            using HttpResponseMessage response = await httpClient.SendAsync(request).ConfigureAwait(false);
+
+            string responseText = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return new MetadataToken(responseText, DateTime.UtcNow + lifetime);
+        }
+
+        private async ValueTask<MetadataToken> GetTokenAsync()
         {
             // does not expire within 5 minutes
             if (token != null && token.Expires > DateTime.UtcNow.AddMinutes(5))
@@ -145,7 +148,7 @@ namespace Amazon.Metadata
             return token;
         }
 
-        public async Task<HttpResponseMessage> GetAsync(string requestUri)
+        private async Task<HttpResponseMessage> GetAsync(string requestUri)
         {
             var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
         
@@ -153,14 +156,14 @@ namespace Amazon.Metadata
 
             request.Headers.Add("X-aws-ec2-metadata-token", token.Value);
 
-            var response = await httpClient.SendAsync(request).ConfigureAwait(false);
+            HttpResponseMessage response = await httpClient.SendAsync(request).ConfigureAwait(false);
 
             return response;
         }
 
-        public async Task<string?> GetStringOrDefaultAsync(string url)
+        private async Task<string?> GetStringOrDefaultAsync(string url)
         {
-            using var response = await GetAsync(url).ConfigureAwait(false);
+            using HttpResponseMessage response = await GetAsync(url).ConfigureAwait(false);
 
             if (response.StatusCode == HttpStatusCode.NotFound)
             {
@@ -170,10 +173,9 @@ namespace Amazon.Metadata
             return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
         }
 
-        public async Task<string> GetStringAsync(string url)
+        private async Task<string> GetStringAsync(string url)
         {
-            using var response = await GetAsync(url).ConfigureAwait(false);
-
+            using HttpResponseMessage response = await GetAsync(url).ConfigureAwait(false);
     
             return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
         }
