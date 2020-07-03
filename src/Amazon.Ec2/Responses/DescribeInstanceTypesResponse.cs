5876512d5774704f6228e04a380c4e2a3b32e4e7
Jason Nelson <jason@carbonmade.com>
2020-07-03 16:43:05 -0500

    [Ec2] Add DescribeInstanceTypesResponse

---
+++ src/Amazon.Ec2/Responses/DescribeInstanceTypesResponse.cs
@@ -0,0 +1,25 @@
+﻿#nullable disable
+
+using System.Xml.Serialization;
+
+namespace Amazon.Ec2
+{
+    [XmlRoot("DescribeInstanceTypesResponse", Namespace = Ec2Client.Namespace)]
+    public sealed class DescribeInstanceTypesResponse
+    {
+        [XmlElement("requestId")]
+        public string RequestId { get; set; }
+
+        [XmlElement("nextToken")]
+        public string NextToken { get; set; }
+
+        [XmlArray("instanceTypeSet")]
+        [XmlArrayItem("item")]
+        public InstanceTypeInfo[] InstanceTypes { get; set; }
+
+        public static DescribeInstanceTypesResponse Deserialize(string text)
+        {
+            return Ec2Serializer<DescribeInstanceTypesResponse>.Deserialize(text);
+        }
+    }
+}
