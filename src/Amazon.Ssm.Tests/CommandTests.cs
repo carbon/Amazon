bd792bbed35e26c0bed80922753080da250bcefa
Jason Nelson <jason@carbonmade.com>
2020-07-03 16:54:40 -0500

    [Ssm] Improve tests

--- src/Amazon.Ssm.Tests/CommandTests.cs
+++ src/Amazon.Ssm.Tests/CommandTests.cs
@@ -1,5 +1,5 @@
 using System;
-using Carbon.Json;
+using System.Text.Json;
 
 using Xunit;
 
@@ -12,10 +12,31 @@ namespace Amazon.Ssm.Tests
         {
             var text = @"{ ""CommandId"":""id"",""Comment"":"""",""CompletedCount"":0,""DocumentName"":""update_app4"",""ErrorCount"":0,""ExpiresAfter"":1.494832672676E9,""InstanceIds"":[],""MaxConcurrency"":""50"",""MaxErrors"":""0"",""NotificationConfig"":{""NotificationArn"":"""",""NotificationEvents"":[],""NotificationType"":""""},""OutputS3BucketName"":"""",""OutputS3KeyPrefix"":"""",""Parameters"":{""appName"":[""platform""]},""RequestedDateTime"":1.494825472676E9,""ServiceRole"":"""",""Status"":""Pending"",""StatusDetails"":""Pending"",""TargetCount"":0,""Targets"":[{""Key"":""tag: envId"",""Values"":[""1""]}]}";
 
-            var command = JsonObject.Parse(text).As<Command>();
+            var command = JsonSerializer.Deserialize<Command>(text);
 
 
-            Assert.Equal(1494825472676, new DateTimeOffset(command.RequestedDateTime).ToUnixTimeMilliseconds());
+            Assert.Equal("id", command.CommandId);
+
+            Assert.Equal("update_app4", command.DocumentName);
+            
+            Assert.Empty(command.Comment);
+            Assert.Equal(0, command.CompletedCount);
+
+            Assert.Equal(CommandStatus.Pending, command.Status);
+            Assert.Equal("Pending", command.StatusDetails);
+
+            Assert.Equal(1494825472676, ((DateTimeOffset)command.RequestedDateTime).ToUnixTimeMilliseconds());
+        }
+
+        [InlineData("InProgress", CommandStatus.InProgress)]
+        [InlineData("Cancelled", CommandStatus.Cancelled)]
+        [Theory]
+        public void ParseStatus(string text, CommandStatus status)
+        {
+            var command = JsonSerializer.Deserialize<Command>(@$"{{ ""Status"":""{text}"" }}");
+
+            Assert.Equal(status, command.Status);
+            Assert.Null(command.RequestedDateTime);
         }
     }
 }
