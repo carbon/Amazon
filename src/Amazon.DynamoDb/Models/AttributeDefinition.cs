fe83e121df0dbda0621dcb76c253d0d39e6ec61c
Jason Nelson <jason@carbonmade.com>
2020-07-16 21:15:13 -0500

    [DynamoDb] Cleanup

--- src/Amazon.DynamoDb/Models/AttributeDefinition.cs
+++ src/Amazon.DynamoDb/Models/AttributeDefinition.cs
@@ -1,15 +1,12 @@
 ﻿#nullable disable
 
-using Amazon.DynamoDb.Extensions;
-using Carbon.Json;
-using System;
-using System.Text.Json;
 
 namespace Amazon.DynamoDb
 {
     public class AttributeDefinition
     {
         public string AttributeName { get; set; }
+
         public AttributeType AttributeType { get; set; }
     }
 }
