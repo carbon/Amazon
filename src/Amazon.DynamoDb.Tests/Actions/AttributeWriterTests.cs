eef304a685d4107696f0afda7d4f0ba52c9c6129
Jason Nelson <jason@carbonmade.com>
2020-08-06 21:54:37 -0500

    Remove final Carbon.Json references

--- src/Amazon.DynamoDb.Tests/Actions/AttributeWriterTests.cs
+++ src/Amazon.DynamoDb.Tests/Actions/AttributeWriterTests.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-
-using Carbon.Json;
+﻿using System.IO;
 
 using Xunit;
 
@@ -9,97 +6,6 @@ namespace Amazon.DynamoDb.Models.Tests
 {
     public class AttributeWriterTests
     {
-        [Fact]
-        public void Json_test_1()
-        {
-            var sb = new StringWriter();
-
-            var writer = new AttributeWriter(sb);
-
-            var json = JsonObject.FromObject(new
-            {
-                a = 1,
-                b = 1.1,
-                c = "a",
-                d = new[] { "a", "b", "c" },
-                e = new List<string> { "a", "b", "c" },
-                f = true,
-                g = false
-            });
-
-            writer.WriteJsonObject(json);
-
-            Assert.Equal(
-                expected: @"{""a"":{""N"":""1""},""b"":{""N"":""1.1""},""c"":{""S"":""a""},""d"":{""L"":[{""S"":""a""},{""S"":""b""},{""S"":""c""}]},""e"":{""L"":[{""S"":""a""},{""S"":""b""},{""S"":""c""}]},""f"":{""BOOL"":true},""g"":{""BOOL"":false}}",
-                actual: sb.ToString()
-            );
-        }
-
-        [Fact]
-        public void Json_test_7()
-        {
-            var sb = new StringWriter();
-
-            var writer = new AttributeWriter(sb);
-
-            var json = JsonObject.FromObject(new
-            {
-                a = new {
-                    a = 1,
-                    b = 1.1,
-                    c = "a",
-                    d = new[] { "a", "b", "c" },
-                    e = new List<string> { "a", "b", "c" },
-                    f = true,
-                    g = false
-                }
-            });
-
-            writer.WriteJsonObject(json);
-
-
-            Assert.Equal(@"{""a"":{""M"":{""a"":{""N"":""1""},""b"":{""N"":""1.1""},""c"":{""S"":""a""},""d"":{""L"":[{""S"":""a""},{""S"":""b""},{""S"":""c""}]},""e"":{""L"":[{""S"":""a""},{""S"":""b""},{""S"":""c""}]},""f"":{""BOOL"":true},""g"":{""BOOL"":false}}}}",
-                actual: sb.ToString());
-        }
-
-        [Fact]
-        public void Json_test_15()
-        {
-            var sb = new StringWriter();
-
-            var writer = new AttributeWriter(sb);
-
-            var json = JsonObject.FromObject(new {
-               a = new[] { 1, 2, 3 },
-            });
-
-            writer.WriteJsonObject(json);
-
-            Assert.Equal(@"{""a"":{""L"":[{""N"":""1""},{""N"":""2""},{""N"":""3""}]}}",
-                actual: sb.ToString());
-        }
-
-        [Fact]
-        public void Json_test_5()
-        {
-            var sb = new StringWriter();
-
-            var writer = new AttributeWriter(sb);
-
-            var json = JsonObject.FromObject(new
-            {
-                a = new HashSet<string>(new[] { "a" })
-            });
-
-            writer.WriteJsonObject(json);
-
-            Assert.Equal(
-                expected: @"{""a"":{""SS"":[""a""]}}",
-                actual: sb.ToString());
-
-
-        }
-
         [Fact]
         public void Json_test_19()
         {
@@ -116,45 +22,6 @@ namespace Amazon.DynamoDb.Models.Tests
             writer.WriteDbValue(value);
 
             Assert.Equal(@"{""M"":{""a"":{""N"":""1""},""b"":{""S"":""boat""}}}", sb.ToString());
-        }
-
-        [Fact]
-        public void Json_test_2()
-        {
-            var sb = new StringWriter();
-
-            var writer = new AttributeWriter(sb);
-
-            var json = JsonObject.FromObject(new
-            {
-                a = new HashSet<string> { "a", "b", "c" },
-            });
-
-            writer.WriteJsonObject(json);
-
-            Assert.Equal(
-                expected: @"{""a"":{""SS"":[""a"",""b"",""c""]}}",
-                actual: sb.ToString());
-        }
-
-
-        [Fact]
-        public void Json_test_3()
-        {
-            var sb = new StringWriter();
-
-            var writer = new AttributeWriter(sb);
-
-            var json = JsonObject.FromObject(new
-            {
-                a = new HashSet<int> { 1, 2, 3 }
-            });
-
-            writer.WriteJsonObject(json);
-
-            Assert.Equal(
-                expected: @"{""a"":{""NS"":[""1"",""2"",""3""]}}",
-                actual: sb.ToString());
         }
     }
 }
--- src/Amazon.DynamoDb.Tests/Helpers/AttributeWriter.cs
+++ src/Amazon.DynamoDb.Tests/Helpers/AttributeWriter.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.Encodings.Web;
 
-using Carbon.Json;
-
 namespace Amazon.DynamoDb.Models
 {
     using static DbValueType;
@@ -16,30 +14,6 @@ namespace Amazon.DynamoDb.Models
         public AttributeWriter(TextWriter writer)
         {
             this.writer = writer;
-        }
-
-        public void WriteJsonObject(JsonObject obj)
-        {
-            int i = 0;
-
-            writer.Write('{');
-            
-            foreach (var property in obj)
-            {
-                if (i != 0) writer.Write(',');
-
-                writer.Write('"');
-                JavaScriptEncoder.Default.Encode(writer, property.Key);
-                writer.Write('"');
-                
-                writer.Write(':');
-
-                WriteXNode(property.Value);
-
-                i++;
-            }
-
-            writer.Write('}');
         }
 
         public void WriteProperty(string name, in DbValue value)
@@ -68,49 +42,6 @@ namespace Amazon.DynamoDb.Models
                 case N    : WriteValue("N", value.ToString());          break;
                 case M    : WriteMap((AttributeCollection)value.Value); break;
                 default   : throw new Exception("Invalid type:" + value.Kind);
-            }
-        }
-      
-
-       
-        public void WriteXNode(JsonNode value)
-        {
-            switch (value.Type)
-            {
-                case JsonType.Number  : WriteValue("N", value.ToString());     break;
-                case JsonType.String  : WriteString(value.ToString());         break;
-                case JsonType.Binary  : WriteValue("B", value.ToString());     break;
-                case JsonType.Boolean : WriteBool(((JsonBoolean)value).Value); break;
-
-                case JsonType.Array:
-                    var array = (JsonArray)value;
-
-                    if (array.IsSet && array.ElementType != null)
-                    {
-                        switch (array.ElementType.Value)
-                        {
-                            case JsonType.Number: WriteSet("NS", array); break;
-                            case JsonType.Binary: WriteSet("BS", array); break;
-                            case JsonType.String: WriteSet("SS", array); break;
-                        }
-                    }
-                    else
-                    {
-                        WriteList(array);
-                    }
-
-                    break;
-
-                case JsonType.Object:
-                    {
-                        writer.Write(@"{""M"":");
-
-                        WriteJsonObject((JsonObject)value);
-
-                        writer.Write('}');
-
-                        break;
-                    }
             }
         }
 
@@ -144,25 +75,6 @@ namespace Amazon.DynamoDb.Models
                 ref DbValue value = ref values[i];
 
                 WriteDbValue(value);
-            }
-
-            writer.Write(@"]}");
-        }
-
-        private void WriteList(IEnumerable<JsonNode> values)
-        {
-            // { "SS":[] }
-            writer.Write(@"{""L"":[");
-
-            int i = 0;
-
-            foreach (var value in values)
-            {
-                if (i != 0) writer.Write(',');
-
-                WriteXNode(value);
-
-                i++;
             }
 
             writer.Write(@"]}");
