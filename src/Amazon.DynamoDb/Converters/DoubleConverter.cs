9b815073e4736ed120d30f60a2d0af42af8e1a9e
Jason Nelson <jason@carbonmade.com>
2020-09-09 16:50:54 -0500

    Format DoubleConverter

--- src/Amazon.DynamoDb/Converters/DoubleConverter.cs
+++ src/Amazon.DynamoDb/Converters/DoubleConverter.cs
@@ -4,10 +4,8 @@ namespace Amazon.DynamoDb
 {
     internal sealed class DoubleConverter : IDbValueConverter
     {
-        public DbValue FromObject(object value, IMember member) =>
-            new DbValue((double)value);
+        public DbValue FromObject(object value, IMember member) => new DbValue((double)value);
 
-        public object ToObject(DbValue item, IMember member) =>
-            item.ToDouble();
+        public object ToObject(DbValue item, IMember member) => item.ToDouble();
     }
 }
