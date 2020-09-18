a06638f663547d06bde9ee29f70559cbdafe91c2
Jason Nelson <jason@carbonmade.com>
2020-09-18 17:36:28 -0500

    Make S3HeaderNames const

--- src/Amazon.S3/Helpers/S3HeaderNames.cs
+++ src/Amazon.S3/Helpers/S3HeaderNames.cs
@@ -2,19 +2,20 @@
 {
     internal static class S3HeaderNames
     {
-        public static readonly string StorageClass                          = "x-amz-storage-class";
-        public static readonly string VersionId                             = "x-amz-version-id"; 
-        public static readonly string ContentSha256                         = "x-amz-content-sha256";
-        public static readonly string MetadataDirective                     = "x-amz-metadata-directive";
+        public const string StorageClass                          = "x-amz-storage-class";
+        public const string VersionId                             = "x-amz-version-id"; 
+        public const string ContentSha256                         = "x-amz-content-sha256";
+        public const string MetadataDirective                     = "x-amz-metadata-directive";
+        public const string Tagging                               = "x-amz-tagging";
 
-        public static readonly string CopySourceIfMatch                     = "x-amz-copy-source-if-match";
-        public static readonly string CopySourceIfNoneMatch                 = "x-amz-copy-source-if-none-match";
-        public static readonly string CopySourceIfUnmodifiedSince           = "x-amz-copy-source-if-unmodified-since";
-        public static readonly string CopySourceIfModifiedSince             = "x-amz-copy-source-if-modified-since";
+        public const string CopySourceIfMatch                     = "x-amz-copy-source-if-match";
+        public const string CopySourceIfNoneMatch                 = "x-amz-copy-source-if-none-match";
+        public const string CopySourceIfUnmodifiedSince           = "x-amz-copy-source-if-unmodified-since";
+        public const string CopySourceIfModifiedSince             = "x-amz-copy-source-if-modified-since";
 
-        public static readonly string ServerSideEncryptionCustomerAlgorithm = "x-amz-server-side-encryption-customer-algorithm";
-        public static readonly string ServerSideEncryptionCustomerKey       = "x-amz-server-side-encryption-customer-key";
-        public static readonly string ServerSideEncryptionCustomerKeyMD5    = "x-amz-server-side-encryption-customer-key-MD5";
+        public const string ServerSideEncryptionCustomerAlgorithm = "x-amz-server-side-encryption-customer-algorithm";
+        public const string ServerSideEncryptionCustomerKey       = "x-amz-server-side-encryption-customer-key";
+        public const string ServerSideEncryptionCustomerKeyMD5    = "x-amz-server-side-encryption-customer-key-MD5";
     }   
 }
 
