3a77eba4a32ee0939967fcaa320b19b213e478cf
Jason Nelson <jason@carbonmade.com>
2020-11-11 23:02:25 -0600

    Add ServiceUnavailableException

---
+++ src/Amazon.Core/Exceptions/ServiceUnavailableException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Amazon.Scheduling;
+
+namespace Amazon.Exceptions
+{
+    public sealed class ServiceUnavailableException : Exception, IException
+    {
+        public ServiceUnavailableException()
+            : base("Service unavailable") { }
+
+        public bool IsTransient => true;
+    }
+}
