67449b92c65b4d4f743fd360fff9356f4d59b750
Jason Nelson <jason@carbonmade.com>
2020-12-22 13:49:03 -0600

    [Sqs] Improve test coverage

---
+++ src/Amazon.Sqs.Tests/SqsMessageTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Amazon.Sqs.Tests
+{
+    public class SqsMessageTests
+    {
+        [Fact]
+        public void TypedMessageTests()
+        {
+            var m = new SqsMessage
+            {
+                Body = @"{ ""position"": 1, ""text"": ""hello"" }"
+            };
+
+            var b = JsonEncodedMessage<SampleMessage>.Create(m);
+
+            Assert.Equal(1, b.Body.Position);
+            Assert.Equal("hello", b.Body.Text);
+        }
+    }
+
+
+    public class SampleMessage
+    {
+        public int Position { get; set; }
+
+        public string Text { get; set; }
+    }
+}
--- src/Amazon.Sqs.Tests/SqsTests.cs
+++ src/Amazon.Sqs.Tests/SqsTests.cs
@@ -2,32 +2,12 @@
 
 namespace Amazon.Sqs.Tests
 {
-    public class SQSTests
+    public class SqsClientTests
     {
         [Fact]
         public void VersionTest()
         {
             Assert.Equal("2012-11-05", SqsClient.Version);
         }
-
-        [Fact]
-        public void TypedMessageTests()
-        {
-            var m = new SqsMessage {
-                Body = @"{ ""position"": 1, ""text"": ""hello"" }"
-            };
-
-            var b = JsonEncodedMessage<SampleMessage>.Create(m);
-
-            Assert.Equal(1, b.Body.Position);
-            Assert.Equal("hello", b.Body.Text);
-        }
-    }
-
-    public class SampleMessage
-    {
-        public int Position { get; set; }
-
-        public string Text { get; set; }
     }
 }
