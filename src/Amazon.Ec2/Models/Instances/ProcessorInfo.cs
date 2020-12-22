d263bcdd2d78c36e3bb4ac0e716140d954c5a0ee
Jason Nelson <jason@carbonmade.com>
2020-12-22 13:07:31 -0600

    [Ec2] Add ProcessorInfo.sustainedClockSpeedInGhz

--- src/Amazon.Ec2/Models/Instances/ProcessorInfo.cs
+++ src/Amazon.Ec2/Models/Instances/ProcessorInfo.cs
@@ -1,13 +1,14 @@
-﻿#nullable disable
-
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace Amazon.Ec2
 {
     public sealed class ProcessorInfo
     {
+        [XmlElement("sustainedClockSpeedInGhz")]
+        public double? SustainedClockSpeedInGhz { get; set; }
+
         [XmlArray("supportedArchitectures")]
         [XmlArrayItem("item")]
-        public string[] SupportedArchitectures { get; set; }
+        public string[]? SupportedArchitectures { get; set; }
     }
 }
