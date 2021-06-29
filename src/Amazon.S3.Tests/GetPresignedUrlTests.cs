eb5a48c2d999caa989c2dec78921833e81538d74
Jason Nelson <jason@carbonmade.com>
2021-06-29 16:15:05 -0500

    [S3] Add stress test for GetPresignedUrl

--- src/Amazon.S3.Tests/GetPresignedUrlTests.cs
+++ src/Amazon.S3.Tests/GetPresignedUrlTests.cs
@@ -20,7 +20,24 @@ namespace Amazon.S3.Tests
             );
 
             Assert.Equal("https://us-east-1.s3.aws.com/test/hi.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=test%2F20000101%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20000101T000000Z&X-Amz-Expires=600&X-Amz-SignedHeaders=host&X-Amz-Signature=55cb96b2f5a8c36b168294b3dc1081b939b1674ca89c97deced6e31b93ec5be2", url);
+        }
+      
+        [Fact]
+        public void StressGetPresignedUrl()
+        {
+            var date = new DateTime(2000, 01, 01, 00, 00, 00, DateTimeKind.Utc);
+            string expect = "https://us-east-1.s3.aws.com/test/hi.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=test%2F20000101%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20000101T000000Z&X-Amz-Expires=600&X-Amz-SignedHeaders=host&X-Amz-Signature=55cb96b2f5a8c36b168294b3dc1081b939b1674ca89c97deced6e31b93ec5be2";
 
+            var request = new GetPresignedUrlRequest("GET", "us-east-1.s3.aws.com", AwsRegion.USEast1, "test", "hi.txt", TimeSpan.FromMinutes(10));
+
+            // 2.2s
+
+            for (int i = 0; i < 100_000; i++)
+            {
+                string url = S3Helper.GetPresignedUrl(request, credential, date);
+
+                Assert.Equal(expect, url);
+            }
         }
 
         [Fact]
