0ce0d3ce773e9a711f126ecc8ac3e686cf162437
Jason Nelson <jason@carbonmade.com>
2021-09-17 16:51:45 -0700

    [KMS] Improve test coverage

---
+++ src/Amazon.Kms.Tests/Exceptions/KmsExceptionTests.cs
@@ -0,0 +1,32 @@
+﻿#pragma warning disable IDE0150 // Prefer 'null' check over type check
+
+using System.Net;
+
+using Amazon.Exceptions;
+
+using Xunit;
+
+namespace Amazon.Kms.Exceptions.Tests
+{
+    public class KmsExceptionTests
+    {
+        [Fact]
+        public void Construct()
+        {
+            var error = new KmsError {
+                Type = "KeyUnavailable", 
+                Message = "The key is not available" 
+            };
+
+            var ex = new KeyUnavailableException(error, HttpStatusCode.NotFound);
+
+            Assert.Equal("KeyUnavailable", ex.Type);
+            Assert.Equal("The key is not available", ex.Message);
+            Assert.Equal(HttpStatusCode.NotFound, ex.HttpStatusCode);
+
+            Assert.True(ex is AwsException);
+
+            Assert.True(ex.IsTransient);
+        }
+    }
+}
