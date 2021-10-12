7f4662b772b8650a9dc0c9c504e7dea98d78374d
Jason Nelson <jason@carbonmade.com>
2021-10-12 12:56:05 -0700

    [Ec2] Format EbsBlockDevice

--- src/Amazon.Ec2/Models/EbsBlockDevice.cs
+++ src/Amazon.Ec2/Models/EbsBlockDevice.cs
@@ -2,63 +2,61 @@
 
 using System.Xml.Serialization;
 
-namespace Amazon.Ec2
+namespace Amazon.Ec2;
+
+public sealed class EbsBlockDevice
 {
-    public class EbsBlockDevice
+    public EbsBlockDevice() { }
+
+    public EbsBlockDevice(
+        string volumeId = null,
+        bool? deleteOnTermination = null,
+        bool? encrypted = null,
+        int? iops = null,
+        string snapshotId = null,
+        int? volumeSize = null,
+        string volumeType = null)
     {
-        public EbsBlockDevice() { }
-
-        public EbsBlockDevice(
-            string volumeId = null,
-            bool? deleteOnTermination = null,
-            bool? encrypted = null,
-            int? iops = null,
-            string snapshotId = null,
-            int? volumeSize = null,
-            string volumeType = null)
-        {
-            VolumeId            = volumeId;
-            DeleteOnTermination = deleteOnTermination;
-            Encrypted           = encrypted;
-            Iops                = iops;
-            SnapshotId          = snapshotId;
-            VolumeSize          = volumeSize;
-            VolumeType          = volumeType;
-        }
-
-        [XmlElement("volumeId")]
-        public string VolumeId { get; set; }
-
-        [XmlElement("status")]
-        public string Status { get; set; }
-
-        [XmlElement("attachTime")]
-        public DateTime? AttachTime { get; set; }
-
-        [XmlElement("deleteOnTermination")]
-        public bool? DeleteOnTermination { get; set; }
-
-        [XmlElement("encrypted")]
-        public bool? Encrypted { get; set; }
-
-        // [Range(100, 20000)]
-        [XmlElement("iops")]
-        public int? Iops { get; set; }
-
-        [XmlElement("snapshotId")]
-        public string SnapshotId { get; set; }
-
-        // The size of the volume, in GiB.
-        // [Range(1, 16384)]
-        [XmlElement("volumeSize")]
-        public int? VolumeSize { get; set; }
-
-        // gp2, io1, st1, sc1, or standard.
-        [XmlElement("volumeType")]
-        public string VolumeType { get; set; }
+        VolumeId            = volumeId;
+        DeleteOnTermination = deleteOnTermination;
+        Encrypted           = encrypted;
+        Iops                = iops;
+        SnapshotId          = snapshotId;
+        VolumeSize          = volumeSize;
+        VolumeType          = volumeType;
     }
-}
 
+    [XmlElement("volumeId")]
+    public string VolumeId { get; init; }
+
+    [XmlElement("status")]
+    public string Status { get; init; }
+
+    [XmlElement("attachTime")]
+    public DateTime? AttachTime { get; init; }
+
+    [XmlElement("deleteOnTermination")]
+    public bool? DeleteOnTermination { get; init; }
+
+    [XmlElement("encrypted")]
+    public bool? Encrypted { get; init; }
+
+    // [Range(100, 20000)]
+    [XmlElement("iops")]
+    public int? Iops { get; init; }
+
+    [XmlElement("snapshotId")]
+    public string SnapshotId { get; init; }
+
+    // The size of the volume, in GiB.
+    // [Range(1, 16384)]
+    [XmlElement("volumeSize")]
+    public int? VolumeSize { get; init; }
+
+    // gp2, io1, st1, sc1, or standard.
+    [XmlElement("volumeType")]
+    public string VolumeType { get; init; }
+}
 
 /*
 <ebs>
