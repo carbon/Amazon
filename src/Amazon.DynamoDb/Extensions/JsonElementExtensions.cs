27902e988e69e701e6af74f89acfdca42db5b43b
Jason Nelson <jason@carbonmade.com>
2021-10-17 14:20:40 -0700

    [DynamoDb] Remove dead code

--- src/Amazon.DynamoDb/Extensions/JsonElementExtensions.cs
+++ src/Amazon.DynamoDb/Extensions/JsonElementExtensions.cs
@@ -1,11 +1,10 @@
-﻿using System.ComponentModel;
-using System.Text.Json;
+﻿using System.Text.Json;
 
 namespace Amazon.DynamoDb.Extensions;
 
 internal static class JsonElementExtensions
 {
-    public static string[] GetStringArray(this JsonElement element)
+    public static string[] GetStringArray(this in JsonElement element)
     {
         var items = new string[element.GetArrayLength()];
 
@@ -19,15 +18,5 @@ internal static class JsonElementExtensions
         }
 
         return items;
-    }
-
-    public static T GetEnum<T>(this JsonElement element) where T : struct, Enum
-    {
-        if (Enum.TryParse(element.GetString(), out T enumValue))
-        {
-            return enumValue;
-        }
-
-        throw new InvalidEnumArgumentException($"{element.GetString()} could not be parsed as enum {typeof(T)}");
     }
 }
