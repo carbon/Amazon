0f10ae12566ebc08bd72e0189b5ef4f80e1b64eb
Jason Nelson <jason@carbonmade.com>
2021-10-22 14:02:17 -0700

    [Sqs] Format SqsMessageAttributeValue

--- src/Amazon.Sqs/Models/SqsMessageAttributeValue.cs
+++ src/Amazon.Sqs/Models/SqsMessageAttributeValue.cs
@@ -2,17 +2,16 @@
 
 using System.Xml.Serialization;
 
-namespace Amazon.Sqs
+namespace Amazon.Sqs;
+
+public sealed class SqsMessageAttributeValue
 {
-    public sealed class SqsMessageAttributeValue
-    {
-        [XmlElement("StringValue")]
-        public string StringValue { get; init; }
+    [XmlElement("StringValue")]
+    public string StringValue { get; init; }
 
-        [XmlElement("BinaryValue")]
-        public string BinaryValue { get; init; }
+    [XmlElement("BinaryValue")]
+    public string BinaryValue { get; init; }
 
-        [XmlElement("DataType")]
-        public string DataType { get; init; }
-    }
+    [XmlElement("DataType")]
+    public string DataType { get; init; }
 }
