1e00171578134fc2a5c09dfeb1cac80be207e4ea
Jason Nelson <jason@carbonmade.com>
2021-11-10 21:21:16 -0800

    [S3] Update S3Exception to inherit from AwsException

--- src/Amazon.S3/Exceptions/S3Exception.cs
+++ src/Amazon.S3/Exceptions/S3Exception.cs
@@ -1,43 +1,33 @@
 using System.Net;
 
+using Amazon.Exceptions;
 using Amazon.Scheduling;
 
 namespace Amazon.S3;
 
-// TODO: Inhert from AwsException
-
-public sealed class S3Exception : Exception, IException
+public sealed class S3Exception : AwsException, IException
 {
-    public S3Exception(string message, HttpStatusCode statusCode)
-        : base(message)
-    {
-        HttpStatusCode = statusCode;
-    }
+    private readonly S3Error? _error;
 
-    public S3Exception(string message, Exception innerException)
-        : base(message, innerException)
+    public S3Exception(string message, HttpStatusCode statusCode)
+        : base(message, statusCode) { }
+
+    public S3Exception(string message, Exception innerException, HttpStatusCode statusCode)
+        : base(message, innerException, statusCode)
     {
         if (innerException is S3Exception s3Exception)
         {
-            HttpStatusCode = s3Exception.HttpStatusCode;
-            ErrorCode      = s3Exception.ErrorCode;
-            RequestId      = s3Exception.RequestId;
+            _error = s3Exception.Error;
         }
     }
 
     public S3Exception(S3Error error, HttpStatusCode statusCode)
         : this(error.Message, statusCode)
     {
-        ErrorCode      = error.Code;
-        HttpStatusCode = statusCode;
-        RequestId      = error.RequestId;
+        _error = error;
     }
 
-    public HttpStatusCode HttpStatusCode { get; }
-
-    public string? ErrorCode { get; }
-
-    public string? RequestId { get; }
+    public S3Error? Error => _error;
 
     public bool IsTransient => HttpStatusCode is HttpStatusCode.InternalServerError or HttpStatusCode.ServiceUnavailable; // 500 || 503
 }
