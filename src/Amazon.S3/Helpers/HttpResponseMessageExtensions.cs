5a88b31a1051c05552219f97659d485ed7d8fcc6
Jason Nelson <jason@carbonmade.com>
2021-11-14 19:29:47 -0800

    [S3] Remove NET5_0 logic

--- src/Amazon.S3/Helpers/HttpResponseMessageExtensions.cs
+++ src/Amazon.S3/Helpers/HttpResponseMessageExtensions.cs
@@ -1,44 +1,26 @@
-﻿using System.Collections.Generic;
-using System.Net.Http;
+﻿using System.Net.Http;
 
-namespace Amazon.S3
+namespace Amazon.S3;
+
+internal static class HttpResponseMessageExtensions
 {
-    internal static class HttpResponseMessageExtensions
+    public static Dictionary<string, string> GetProperties(this HttpResponseMessage response)
     {
-        public static Dictionary<string, string> GetProperties(this HttpResponseMessage response)
+        var baseHeaders = response.Headers.NonValidated;
+        var contentHeaders = response.Content.Headers.NonValidated;
+
+        var result = new Dictionary<string, string>(baseHeaders.Count + contentHeaders.Count);
+
+        foreach (var header in baseHeaders)
         {
-#if NET5_0
-            var result = new Dictionary<string, string>(16);
-
-            foreach (var header in response.Headers)
-            {
-                result.Add(header.Key, string.Join(';', header.Value));
-            }
-
-            foreach (var header in response.Content.Headers)
-            {
-                result.Add(header.Key, string.Join(';', header.Value));
-            }
-
-
-#else
-            var baseHeaders = response.Headers.NonValidated;
-            var contentHeaders = response.Content.Headers.NonValidated;
-
-            var result = new Dictionary<string, string>(baseHeaders.Count + contentHeaders.Count);
-
-            foreach (var header in baseHeaders)
-            {
-                result.Add(header.Key, header.Value.ToString());
-            }
-
-            foreach (var header in contentHeaders)
-            {
-                result.Add(header.Key, header.Value.ToString());
-            }
-#endif
-
-            return result;
+            result.Add(header.Key, header.Value.ToString());
         }
+
+        foreach (var header in contentHeaders)
+        {
+            result.Add(header.Key, header.Value.ToString());
+        }
+
+        return result;
     }
 }
