91c917710bd57785836fdc36c6c1ed7425aeade4
Jason Nelson <jason@carbonmade.com>
2022-01-13 00:15:38 -0800

    [S3] Improve test coverage

--- src/Amazon.S3.Tests/Actions/CopyObjectRequestTests.cs
+++ src/Amazon.S3.Tests/Actions/CopyObjectRequestTests.cs
@@ -14,11 +14,11 @@ public class CopyObjectRequestTests
         Assert.Equal(new Uri("https://s3.amazon.com/targetBucket/video.mp4"), request.RequestUri);
         Assert.Null(request.MetadataDirective);
         Assert.False(request.Headers.Contains("x-amz-metadata-directive"));
-        Assert.Equal("/sourceBucket/video.mp4", request.Headers.GetValues("x-amz-copy-source").FirstOrDefault());
+        Assert.Equal("/sourceBucket/video.mp4", request.Headers.GetValues("x-amz-copy-source").First());
     }
 
     [Fact]
-    public void CanSetMetdataDirective()
+    public void CanSetMetdataDirective_Replace()
     {
         var request = new CopyObjectRequest(
             host   : "s3.amazon.com",
@@ -29,11 +29,27 @@ public class CopyObjectRequestTests
         request.MetadataDirective = MetadataDirectiveValue.Replace;
 
         Assert.Equal("REPLACE", request.Headers.GetValues("x-amz-metadata-directive").First());
-
         Assert.Equal(MetadataDirectiveValue.Replace, request.MetadataDirective);
 
         request.MetadataDirective = null;
 
         Assert.Null(request.MetadataDirective);
+
+        Assert.False(request.Headers.Contains("x-amz-metadata-directive"));
+    }
+
+    [Fact]
+    public void CanSetMetdataDirective_Copy()
+    {
+        var request = new CopyObjectRequest(
+            host: "s3.amazon.com",
+            source: new S3ObjectLocation("sourceBucket", "video.mp4"),
+            target: new S3ObjectLocation("targetBucket", "video.mp4")
+        );
+
+        request.MetadataDirective = MetadataDirectiveValue.Copy;
+
+        Assert.Equal("COPY",                      request.Headers.NonValidated["x-amz-metadata-directive"].ToString());
+        Assert.Equal(MetadataDirectiveValue.Copy, request.MetadataDirective);
     }
 }
---
+++ src/Amazon.S3.Tests/Actions/GetObjectRequestTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Amazon.S3.Actions.Tests;
+
+public class GetObjectRequestTests
+{
+    [Fact]
+    public void SetRange()
+    {
+        var request = new GetObjectRequest("s3.amazon.com", "bucket", "objectKey");
+
+        request.SetRange(int.MaxValue, 9147484647);
+        
+        Assert.Equal("bytes=2147483647-9147484647", request.Headers.NonValidated["Range"].ToString());
+    }
+
+    [Fact]
+    public void SetRange2()
+    {
+        var request = new GetObjectRequest("s3.amazon.com", "bucket", "objectKey");
+
+        request.SetRange(1325400064, 1342177279);
+        Assert.Equal("bytes=1325400064-1342177279", request.Headers.NonValidated["Range"].ToString());
+    }
+}
