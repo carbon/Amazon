3cb3e7f385315a3f548b2417a7b98ff8dc685681
Jason Nelson <jason@carbonmade.com>
2022-01-26 23:57:40 -0800

    [S3] Eliminate allocation computing MD5 hash

--- src/Amazon.S3/Helpers/HashHelper.cs
+++ src/Amazon.S3/Helpers/HashHelper.cs
@@ -1,12 +1,21 @@
-﻿using System.Security.Cryptography;
+﻿using System.Buffers;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Amazon.S3;
 
 internal static class HashHelper
 {
-    public static byte[] ComputeMD5Hash(string text)
+    public static byte[] ComputeMD5Hash(ReadOnlySpan<char> text)
     {
-        return MD5.HashData(Encoding.UTF8.GetBytes(text));
+        byte[] utf8Buffer = ArrayPool<byte>.Shared.Rent(text.Length * 4);
+
+        int bufferLength = Encoding.UTF8.GetBytes(text, utf8Buffer);
+
+        var result = MD5.HashData(utf8Buffer.AsSpan(0, bufferLength));
+
+        ArrayPool<byte>.Shared.Return(utf8Buffer);
+
+        return result;
     }
 }
