033eefd955e2f2aeb1364fedb01ca26b9aaa65b4
Jason Nelson <jason@carbonmade.com>
2022-03-01 21:31:38 -0800

    [Ses] Fix header names used in EmailMessageDecryptor

--- src/Amazon.Ses.Extensions/EmailMessageDecryptor.cs
+++ src/Amazon.Ses.Extensions/EmailMessageDecryptor.cs
@@ -19,19 +19,19 @@ public sealed class EmailMessageDecryptor
     {
         ArgumentNullException.ThrowIfNull(encryptedBlob);
 
-        var cekAlg = encryptedBlob.Properties["x-amz-cek-alg"];
+        var cekAlg = encryptedBlob.Properties["x-amz-meta-x-amz-cek-alg"];
 
         if (cekAlg is not "AES/GCM/NoPadding")
         {
             throw new Exception($"Expected AES/GCM/NoPadding. Was '{cekAlg}'");
         }
 
-        var encryptionContext = JsonSerializer.Deserialize<Dictionary<string, string>>(encryptedBlob.Properties["x-amz-matdesc"])!;
+        var encryptionContext = JsonSerializer.Deserialize<Dictionary<string, string>>(encryptedBlob.Properties["x-amz-meta-x-amz-matdesc"])!;
 
-        byte[] envelopeKey = Convert.FromBase64String(encryptedBlob.Properties["x-amz-key-v2"]);
-        byte[] envelopeIV  = Convert.FromBase64String(encryptedBlob.Properties["x-amz-iv"]);
-        long contentLength = long.Parse(encryptedBlob.Properties["x-amz-unencrypted-content-length"]);
-        int tagLength      = int.Parse(encryptedBlob.Properties["x-amz-tag-len"]);
+        byte[] envelopeKey = Convert.FromBase64String(encryptedBlob.Properties["x-amz-meta-x-amz-key-v2"]);
+        byte[] envelopeIV  = Convert.FromBase64String(encryptedBlob.Properties["x-amz-meta-x-amz-iv"]);
+        long contentLength = long.Parse(encryptedBlob.Properties["x-amz-meta-x-amz-unencrypted-content-length"]);
+        int tagLength      = int.Parse(encryptedBlob.Properties["x-amz-meta-x-amz-tag-len"]);
 
         if (tagLength != 128)
         {
