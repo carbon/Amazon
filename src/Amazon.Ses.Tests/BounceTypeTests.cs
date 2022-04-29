c25e20cb178b27a1f33e0bf38eb2e51bba8f5b32
Jason Nelson <jason@carbonmade.com>
2022-04-29 14:40:57 -0700

    [SES] Improve test coverage

---
+++ src/Amazon.Ses.Tests/BounceTypeTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Amazon.Ses.Tests;
+
+public sealed class BounceTypeTests
+{
+    [Fact]
+    public void IdsAreFixed()
+    {
+        Assert.Equal(1, (int)SesBounceType.Undetermined);
+        Assert.Equal(2, (int)SesBounceType.Permanent);
+    }
+}
---
+++ src/Amazon.Ses.Tests/SesBounceSubtypeTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Amazon.Ses.Tests;
+
+public sealed class SesBounceSubtypeTests
+{
+    [Fact]
+    public void IdsAreFixed()
+    {
+        Assert.Equal(2, (int)SesBounceSubtype.General);
+        Assert.Equal(8, (int)SesBounceSubtype.AttachmentRejected);
+
+    }
+}
