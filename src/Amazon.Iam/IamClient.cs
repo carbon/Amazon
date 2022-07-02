18dc62dbe5ea7083e5042d574339877a25f14c7b
Jason Nelson <jason@carbonmade.com>
2022-07-01 19:28:12 -0700

    [IAM] Use file scoped namespaces

--- src/Amazon.Iam/IamClient.cs
+++ src/Amazon.Iam/IamClient.cs
@@ -3,63 +3,62 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Amazon.Iam
+namespace Amazon.Iam;
+
+public sealed class IamClient : AwsClient
 {
-    public sealed class IamClient : AwsClient
+    public const string Version = "2010-05-08";
+
+    public IamClient(AwsCredential credential)
+        : base(AwsService.Iam, AwsRegion.USEast1, credential)
+    { }
+
+    public async Task CreateAccessKeyAsync(string userName)
     {
-        public const string Version = "2010-05-08";
+        var parameters = new AwsRequest {
+            { "Action", "CreateAccessKey" },
+            { "UserName", userName }
+        };
 
-        public IamClient(AwsCredential credential)
-            : base(AwsService.Iam, AwsRegion.USEast1, credential)
-        { }
-
-        public async Task CreateAccessKeyAsync(string userName)
-        {
-            var parameters = new AwsRequest {
-                { "Action", "CreateAccessKey" },
-                { "UserName", userName }
-            };
-
-            await SendAsync(parameters).ConfigureAwait(false);
-        }
-
-        public async Task CreateUserAsync(string userName)
-        {
-            var parameters = new AwsRequest {
-                { "Action", "CreateUser" },
-                { "UserName", userName }
-            };
-
-            await SendAsync(parameters).ConfigureAwait(false);
-        }
-
-        public async Task PutUserPolicyAsync(string userName, string policyName, string policyDocument)
-        {
-            var parameters = new AwsRequest {
-                { "Action", "PutUserPolicy" },
-                { "UserName", userName },
-                { "PolicyName", policyName },
-                { "PolicyDocument", policyDocument }
-            };
-
-            await SendAsync(parameters).ConfigureAwait(false);
-        }
-
-        #region Helpers
-
-        private Task<string> SendAsync(AwsRequest request)
-        {
-            request.Add("Version", Version);
-
-            var httpRequest = new HttpRequestMessage(HttpMethod.Post, Endpoint) {
-                Content = new FormUrlEncodedContent(request.Parameters!)
-            };
-
-            return SendAsync(httpRequest);
-        }
-
-        #endregion
+        await SendAsync(parameters).ConfigureAwait(false);
     }
+
+    public async Task CreateUserAsync(string userName)
+    {
+        var parameters = new AwsRequest {
+            { "Action", "CreateUser" },
+            { "UserName", userName }
+        };
+
+        await SendAsync(parameters).ConfigureAwait(false);
+    }
+
+    public async Task PutUserPolicyAsync(string userName, string policyName, string policyDocument)
+    {
+        var parameters = new AwsRequest {
+            { "Action", "PutUserPolicy" },
+            { "UserName", userName },
+            { "PolicyName", policyName },
+            { "PolicyDocument", policyDocument }
+        };
+
+        await SendAsync(parameters).ConfigureAwait(false);
+    }
+
+    #region Helpers
+
+    private Task<string> SendAsync(AwsRequest request)
+    {
+        request.Add("Version", Version);
+
+        var httpRequest = new HttpRequestMessage(HttpMethod.Post, Endpoint) {
+            Content = new FormUrlEncodedContent(request.Parameters!)
+        };
+
+        return SendAsync(httpRequest);
+    }
+
+    #endregion
 }
 
 // http://docs.aws.amazon.com/IAM/latest/APIReference/Welcome.html
