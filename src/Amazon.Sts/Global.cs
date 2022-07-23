d5b90731f1d0e6babe3b80b7f96d387b7b1a9a38
Jason Nelson <jason@carbonmade.com>
2022-07-22 21:14:29 -0700

    [STS] Use global usings

--- src/Amazon.Sts/Global.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly:InternalsVisibleTo("Amazon.Sts.Tests")]
---
+++ src/Amazon.Sts/Globals.cs
@@ -0,0 +1,6 @@
+﻿global using System;
+global using System.Collections.Generic;
+
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Amazon.Sts.Tests")]
