d3706106ab17cd33078936f2e29eba3b334e439e
Jason Nelson <jason@carbonmade.com>
2022-07-22 20:57:16 -0700

    [DynamoDB] Add Timestamp test coverage

---
+++ src/Amazon.DynamoDb.Tests/Actions/TimestampTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Amazon.DynamoDb.Models.Tests;
+
+public class TimestampTests
+{
+    [Fact]
+    public void CanConvertToDateTime()
+    {
+        var timestamp = new Timestamp(1658453070);
+
+        Assert.Equal(1658453070d, timestamp.Value);
+
+        Assert.Equal(new DateTime(2022, 07, 22, 01, 24, 30, DateTimeKind.Utc), timestamp);
+    }
+
+    [Fact]
+    public void CanConvertToDateTimeOffset()
+    {
+        var timestamp = new Timestamp(1658453070);
+
+        Assert.Equal(timestamp.Value, ((DateTimeOffset)timestamp).ToUnixTimeSeconds());
+        Assert.Equal(new DateTimeOffset(2022, 07, 22, 01, 24, 30, TimeSpan.Zero), timestamp);    
+    }
+}
