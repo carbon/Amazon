08042bd1062f1d5e4f6e16f469740c2eb9179e18
Jason Nelson <jason@carbonmade.com>
2022-07-28 21:53:37 -0700

    [Route53] Improve nullable annotations

--- src/Amazon.Route53/Models/ResourceRecordSet.cs
+++ src/Amazon.Route53/Models/ResourceRecordSet.cs
@@ -10,37 +10,38 @@ public sealed class ResourceRecordSet
     public ResourceRecordSet() { }
 
 #nullable enable
-    public ResourceRecordSet(ResourceRecordType type, string name!!, params ResourceRecord[] resourceRecords)
+    public ResourceRecordSet(ResourceRecordType type, string name, params ResourceRecord[] resourceRecords)
     {
+        ArgumentNullException.ThrowIfNull(name);
+
         Type = type;
         Name = name;
         ResourceRecords = resourceRecords;
     }
-#nullable disable
 
-    public AliasTarget AliasTarget { get; init; }
+    public AliasTarget? AliasTarget { get; init; }
 
     [DefaultValue(Failover.None)]
     public Failover Failover { get; init; }
 
-    public GeoLocation GeoLocation { get; init; }
+    public GeoLocation? GeoLocation { get; init; }
 
-    public string HealthCheckId { get; init; }
+    public string? HealthCheckId { get; init; }
 
     [DefaultValue(false)]
     public bool MultiValueAnswer { get; init; }
 
     public string Name { get; init; }
 
-    public string Region { get; init; }
+    public string? Region { get; init; }
 
     [XmlArray("ResourceRecords")]
     [XmlArrayItem("ResourceRecord")]
     public ResourceRecord[] ResourceRecords { get; init; }
 
-    public string SetIdentifier { get; init; }
+    public string? SetIdentifier { get; init; }
 
-    public string TrafficPolicyInstanceId { get; init; }
+    public string? TrafficPolicyInstanceId { get; init; }
 
     [DefaultValue(0)]
     public int TTL { get; init; }
