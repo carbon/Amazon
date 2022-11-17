37153ab3573e08e01ffb1a76dd0cb4a0813562f2
Jason Nelson <jason@carbonmade.com>
2022-11-16 22:59:55 -0800

    [Core] Make SigningParameterNames values const

--- src/Amazon.Core/Security/SigningParameterNames.cs
+++ src/Amazon.Core/Security/SigningParameterNames.cs
@@ -2,11 +2,11 @@
 
 internal static class SigningParameterNames
 {
-    public static readonly string Algorithm     = "X-Amz-Algorithm";
-    public static readonly string Credential    = "X-Amz-Credential";
-    public static readonly string Expires       = "X-Amz-Expires";
-    public static readonly string Date          = "X-Amz-Date";
-    public static readonly string SecurityToken = "X-Amz-Security-Token";
-    public static readonly string SignedHeaders = "X-Amz-SignedHeaders";
-    public static readonly string Signature     = "X-Amz-Signature";
+    public const string Algorithm     = "X-Amz-Algorithm";
+    public const string Credential    = "X-Amz-Credential";
+    public const string Expires       = "X-Amz-Expires";
+    public const string Date          = "X-Amz-Date";
+    public const string SecurityToken = "X-Amz-Security-Token";
+    public const string SignedHeaders = "X-Amz-SignedHeaders";
+    public const string Signature     = "X-Amz-Signature";
 }
