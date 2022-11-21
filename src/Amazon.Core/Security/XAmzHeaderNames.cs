873c1c11acc3f6e82c3f022eb959cc1aafd040c3
Jason Nelson <jason@carbonmade.com>
2022-11-20 18:12:25 -0800

    [Core] Add XAmzHeaderNames

---
+++ src/Amazon.Core/Security/XAmzHeaderNames.cs
@@ -0,0 +1,11 @@
+﻿namespace Amazon.Security;
+
+internal static class XAmzHeaderNames
+{
+    public const string ContentSHA256        = "x-amz-content-sha256";
+    public const string DecodedContentLength = "x-amz-decoded-content-length";
+    public const string ChecksumCrc32        = "x-amz-checksum-crc32";
+    public const string TrailerSignature     = "x-amz-trailer-signature";
+    public const string Date                 = "x-amz-date";
+    public const string SecurityToken        = "x-amz-security-token";
+}
