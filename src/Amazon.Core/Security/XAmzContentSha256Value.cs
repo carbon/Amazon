98230981aac505c207e142823c5935e6b29be83e
Jason Nelson <jason@carbonmade.com>
2022-11-20 18:18:01 -0800

    [Core] Add XAmzContentSha256Values

---
+++ src/Amazon.Core/Security/XAmzContentSha256Value.cs
@@ -0,0 +1,9 @@
+﻿namespace Amazon;
+
+internal static class XAmzContentSha256Values
+{
+    public const string UnsignedPayload               = "UNSIGNED-PAYLOAD";
+    public const string StreamingAwsHmacSha256Payload = "STREAMING-AWS4-HMAC-SHA256-PAYLOAD";
+}
+
+// https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-streaming-trailers.html
