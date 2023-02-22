ae2238e9803892c37c2147d4103223af0c11bcbf
Jason Nelson <jason@carbonmade.com>
2023-02-21 16:08:20 -0800

    [Core] Add eu-south-2, ap-south-2, and ap-southeast-4

--- src/Amazon.Core/Models/AwsRegion.cs
+++ src/Amazon.Core/Models/AwsRegion.cs
@@ -40,31 +40,38 @@ public sealed class AwsRegion : IEquatable<AwsRegion>
 
     #endregion
 
+    // AP = Asia Pacific
+    // CA = Canada
+    // CN = China
+    // SA = South America
+
     // In launch order...
-    public static readonly AwsRegion USEast1      = new("us-east-1");      // | US            | N. Virginia   | 2006-08-25
-    public static readonly AwsRegion EUWest1      = new("eu-west-1");      // | EU            | Ireland       | 2008-12-10
-    public static readonly AwsRegion USWest1      = new("us-west-1");      // | US            | N. California | 2009-12-03
-    public static readonly AwsRegion APSouthEast1 = new("ap-southeast-1"); // | Asia Pacific  | Singapore     | 2010-04-29
-    public static readonly AwsRegion APNorthEast1 = new("ap-northeast-1"); // | Asia Pacific  | Tokyo         | 2011-03-02
-    public static readonly AwsRegion USGovWest1   = new("us-gov-west-1");  // | US            | AWS GovCloud  | 2011-08-16
-    public static readonly AwsRegion USWest2      = new("us-west-2");      // | US            | Oregon        | 2011-11-09
-    public static readonly AwsRegion SAEast1      = new("sa-east-1");      // | South America | São Paulo     | 2011-12-14
-    public static readonly AwsRegion APSouthEast2 = new("ap-southeast-2"); // | Asia Pacific  | Sydney        | 2012-11-12
-    public static readonly AwsRegion CNNorth1     = new("cn-north-1");     // | China         | Beijing       | 2013-12-18
-    public static readonly AwsRegion EUCentral1   = new("eu-central-1");   // | EU            | Frankfurt     | 2014-10-23
-    public static readonly AwsRegion APNortheast2 = new("ap-northeast-2"); // | Asia Pacific  | Seoul         | 2016-01-06
-    public static readonly AwsRegion APSouth1     = new("ap-south-1");     // | Asia Pacific  | Mumbai        | 2016-06-27
-    public static readonly AwsRegion USEast2      = new("us-east-2");      // | US            | Ohio          | 2016-10-17
-    public static readonly AwsRegion CACentral1   = new("ca-central-1");   // | Canada        | Central       | 2016-12-08
-    public static readonly AwsRegion EUWest2      = new("eu-west-2");      // | EU            | London        | 2016-12-13
-    public static readonly AwsRegion EUWest3      = new("eu-west-3");      // | EU            | Paris         | 2017-12-18
-    public static readonly AwsRegion MESouth1     = new("me-south-1");     // | ME            | Bahrain       | 2019-07-29
-    public static readonly AwsRegion AFSouth1     = new("af-south-1");     // |               | Cape Town     | ~2020-04
-    public static readonly AwsRegion EUSouth1     = new("eu-south-1");     // | EU            | Milan         | ~2020-05	
-    public static readonly AwsRegion APSouthEast3 = new("ap-southeast-3"); // | Asia Pacific  | Jakarta       | ~2021-11
-    public static readonly AwsRegion MECentral1   = new("me-central-1");   // | ME            | UAE           | 2022-09
-    public static readonly AwsRegion EUCentral2   = new("eu-central-2");   // | EU            | Switzerland   | 2022-11   
-    public static readonly AwsRegion EUSouth2     = new("eu-south-2");     // | EU            | Spain
+    public static readonly AwsRegion USEast1      = new("us-east-1");      // N. Virginia    | 2006-08-25
+    public static readonly AwsRegion EUWest1      = new("eu-west-1");      // Ireland        | 2008-12-10
+    public static readonly AwsRegion USWest1      = new("us-west-1");      // N. California  | 2009-12-03
+    public static readonly AwsRegion APSouthEast1 = new("ap-southeast-1"); // Singapore      | 2010-04-29
+    public static readonly AwsRegion APNorthEast1 = new("ap-northeast-1"); // Tokyo          | 2011-03-02
+    public static readonly AwsRegion USGovWest1   = new("us-gov-west-1");  // AWS GovCloud   | 2011-08-16
+    public static readonly AwsRegion USWest2      = new("us-west-2");      // Oregon         | 2011-11-09
+    public static readonly AwsRegion SAEast1      = new("sa-east-1");      // São Paulo      | 2011-12-14
+    public static readonly AwsRegion APSouthEast2 = new("ap-southeast-2"); // Sydney         | 2012-11-12
+    public static readonly AwsRegion CNNorth1     = new("cn-north-1");     // Beijing        | 2013-12-18
+    public static readonly AwsRegion EUCentral1   = new("eu-central-1");   // Frankfurt      | 2014-10-23
+    public static readonly AwsRegion APNortheast2 = new("ap-northeast-2"); // Seoul          | 2016-01-06
+    public static readonly AwsRegion APSouth1     = new("ap-south-1");     // Mumbai         | 2016-06-27
+    public static readonly AwsRegion USEast2      = new("us-east-2");      // Ohio           | 2016-10-17
+    public static readonly AwsRegion CACentral1   = new("ca-central-1");   // Central        | 2016-12-08
+    public static readonly AwsRegion EUWest2      = new("eu-west-2");      // London         | 2016-12-13
+    public static readonly AwsRegion EUWest3      = new("eu-west-3");      // Paris          | 2017-12-18
+    public static readonly AwsRegion MESouth1     = new("me-south-1");     // Bahrain        | 2019-07-29
+    public static readonly AwsRegion AFSouth1     = new("af-south-1");     // Cape Town      | ~2020-04
+    public static readonly AwsRegion EUSouth1     = new("eu-south-1");     // Milan          | ~2020-05	
+    public static readonly AwsRegion APSouthEast3 = new("ap-southeast-3"); // Jakarta        | ~2021-11
+    public static readonly AwsRegion MECentral1   = new("me-central-1");   // UAE            | 2022-09
+    public static readonly AwsRegion EUCentral2   = new("eu-central-2");   // Switzerland    | 2022-11   
+    public static readonly AwsRegion EUSouth2     = new("eu-south-2");     // Spain          | 2022-11-15
+    public static readonly AwsRegion APSouth2     = new("ap-south-2");     // Hyderabad      | 2022-11-21
+    public static readonly AwsRegion APSouthEast4 = new("ap-southeast-4"); // Melbourne (AU) | 2023-01-23
 
     public static readonly AwsRegion[] All = new [] {
         USEast1,
@@ -90,15 +97,19 @@ public sealed class AwsRegion : IEquatable<AwsRegion>
         APSouthEast3,
         MECentral1,
         EUCentral2,
-        EUSouth2
+        EUSouth2,
+        APSouth2,
+        APSouthEast4
     };
         
     public static AwsRegion Get(string name) => name switch
     {
         "ap-south-1"     => APSouth1,
+        "ap-south-2"     => APSouth2,
         "ap-southeast-1" => APSouthEast1,
         "ap-southeast-2" => APSouthEast2,
         "ap-southeast-3" => APSouthEast3,
+        "ap-southeast-4" => APSouthEast4,
         "ap-northeast-1" => APNorthEast1,
         "ap-northeast-2" => APNortheast2,
         "us-east-1"      => USEast1,
@@ -157,7 +168,7 @@ public sealed class AwsRegion : IEquatable<AwsRegion>
 
     public static implicit operator AwsRegion(string name)
     {
-        ArgumentNullException.ThrowIfNull(name);
+        ArgumentException.ThrowIfNullOrEmpty(name);
 
         return new AwsRegion(name);
     }
