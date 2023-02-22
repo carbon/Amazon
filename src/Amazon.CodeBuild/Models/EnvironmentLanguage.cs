2680ec6904531b984e64a7340911a8928797d914
Jason Nelson <jason@carbonmade.com>
2023-02-21 16:07:17 -0800

    [CodeBuild] Add Language enum

--- src/Amazon.CodeBuild/Models/EnvironmentLanguage.cs
+++ src/Amazon.CodeBuild/Models/EnvironmentLanguage.cs
@@ -6,6 +6,5 @@ public sealed class EnvironmentLanguage
 {
     public EnvironmentImage[] Images { get; init; }
 
-    // JAVA | PYTHON | NODE_JS | RUBY | GOLANG | DOCKER | ANDROID | BASE
-    public string Language { get; init; }
+    public Language Language { get; init; }
 }
---
+++ src/Amazon.CodeBuild/Models/Language.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+
+namespace Amazon.CodeBuild;
+
+public enum Language
+{
+    JAVA,
+    PYTHON,
+    NODE_JS,
+    RUBY,
+    GOLANG,
+    DOCKER,
+    ANDROID,
+    DOTNET,
+    BASE,
+    PHP
+}
