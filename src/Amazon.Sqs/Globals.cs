960699bd3141a24ae48970117952ba75be7cb0b3
Jason Nelson <jason@carbonmade.com>
2023-04-20 13:40:13 -0700

    [Sqs] Make internals visible to tests

--- src/Amazon.Sqs/Globals.cs
+++ src/Amazon.Sqs/Globals.cs
@@ -1,3 +1,7 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Threading.Tasks;
+
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Amazon.Sqs.Tests")]
