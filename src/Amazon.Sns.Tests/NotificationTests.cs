0507601b460a39405c198edc8fad62853d32a740
Jason Nelson <jason@carbonmade.com>
2024-01-26 12:17:11 -0800

    [SNS] Add test coverage

---
+++ src/Amazon.Sns.Tests/NotificationTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+namespace Amazon.Sns.Tests;
+
+public class NotificationTests
+{
+    [Fact]
+    public void CanDeserialize()
+    {
+        var message = JsonSerializer.Deserialize<SnsMessage>(
+            """
+            {
+              "Type" : "Notification",
+              "MessageId" : "22b80b92-fdea-4c2c-8f9d-bdfb0c7bf324",
+              "TopicArn" : "arn:aws:sns:us-west-2:123456789012:MyTopic",
+              "Subject" : "My First Message",
+              "Message" : "Hello world!",
+              "Timestamp" : "2012-05-02T00:54:06.655Z",
+              "SignatureVersion" : "1",
+              "Signature" : "EXAMPLEw6JRN...",
+              "SigningCertURL" : "https://sns.us-west-2.amazonaws.com/SimpleNotificationService-f3ecfb7224c7233fe7bb5f59f96de52f.pem",
+              "UnsubscribeURL" : "https://sns.us-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-west-2:123456789012:MyTopic:c9135db0-26c4-47ec-8998-413945fb5a96"
+            }
+            """);
+
+        Assert.NotNull(message);
+        Assert.Equal("22b80b92-fdea-4c2c-8f9d-bdfb0c7bf324",        message.MessageId);
+        Assert.Equal("arn:aws:sns:us-west-2:123456789012:MyTopic",  message.TopicArn);
+        Assert.Equal("EXAMPLEw6JRN...",                             message.Signature);
+        Assert.Equal("1",                                           message.SignatureVersion);
+
+        var notification = (Notification)message;
+
+        Assert.Equal("Hello world!", notification.Message);
+    }
+}
---
+++ src/Amazon.Sns.Tests/UnsubscribeConfirmationTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace Amazon.Sns.Tests;
+
+public class UnsubscribeConfirmationTests
+{
+    [Fact]
+    public void CanDeserialize()
+    {
+        var notification = JsonSerializer.Deserialize<SnsMessage>(
+            """                        
+            {
+              "Type" : "UnsubscribeConfirmation",
+              "MessageId" : "47138184-6831-46b8-8f7c-afc488602d7d",
+              "Token" : "2336412f37...",
+              "TopicArn" : "arn:aws:sns:us-west-2:123456789012:MyTopic",
+              "Message" : "You have chosen to deactivate subscription arn:aws:sns:us-west-2:123456789012:MyTopic:2bcfbf39-05c3-41de-beaa-fcfcc21c8f55.\nTo cancel this operation and restore the subscription, visit the SubscribeURL included in this message.",
+              "SubscribeURL" : "https://sns.us-west-2.amazonaws.com/?Action=ConfirmSubscription&TopicArn=arn:aws:sns:us-west-2:123456789012:MyTopic&Token=2336412f37fb6...",
+              "Timestamp" : "2012-04-26T20:06:41.581Z",
+              "SignatureVersion" : "1",
+              "Signature" : "EXAMPLEHXgJm...",
+              "SigningCertURL" : "https://sns.us-west-2.amazonaws.com/SimpleNotificationService-f3ecfb7224c7233fe7bb5f59f96de52f.pem"
+            }
+            """);
+
+        Assert.True(notification is UnsubscribeConfirmation);
+
+        Assert.Equal("47138184-6831-46b8-8f7c-afc488602d7d", notification.MessageId);
+    }
+}
