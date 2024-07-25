5ca226dbd5263b896709d12789c528a4339b297f
Jason Nelson <jason@carbonmade.com>
2024-07-24 21:59:56 -0700

    Fix TranscribeSerializerContext

--- src/Amazon.Transcribe/Serialization/TranscribeSerializerContext.cs
+++ src/Amazon.Transcribe/Serialization/TranscribeSerializerContext.cs
@@ -2,8 +2,10 @@
 
 namespace Amazon.Transcribe.Serialization;
 
+[JsonSerializable(typeof(GetTranscriptionJobRequest))]
+[JsonSerializable(typeof(GetTranscriptionJobResult))]
 [JsonSerializable(typeof(StartTranscriptionJobRequest))]
 [JsonSerializable(typeof(StartTranscriptionJobResult))]
-public partial class TranscribeerializerContext : JsonSerializerContext
+public partial class TranscribeSerializerContext : JsonSerializerContext
 {
 }
