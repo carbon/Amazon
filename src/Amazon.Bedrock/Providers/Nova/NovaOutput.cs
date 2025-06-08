de2afc671dd5ff256852a5288322f218c648e5e2
Jason Nelson <jason@carbonmade.com>
2025-06-08 13:42:58 -0700

    [Bedrock] Remove Nova specific models (prefer converse models)

--- src/Amazon.Bedrock/Providers/Nova/NovaOutput.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Amazon.Nova;
-
-public sealed class NovaOutput
-{
-    [JsonPropertyName("message")]
-    public required NovaMessage Message { get; init; }
-}
