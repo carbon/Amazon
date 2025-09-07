df22ac2f069108bf17ffd9ee42858a33b096c440
Jason Nelson <jason@carbonmade.com>
2025-09-06 17:47:14 -0700

    [Cryptography] Add EncryptMessageRequest and DataKey

---
+++ src/Amazon.Cryptography/Actions/EncryptMessageRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Amazon.Cryptography;
+
+public sealed class EncryptMessageRequest
+{
+    public required AlgorithmSuiteId AlgorithmId { get; init; }
+
+    public required EncryptionContext EncryptionContext { get; init; }
+
+    public uint FrameLength { get; } = 4096;
+
+    public required byte[] Plaintext { get; init; }
+}
---
+++ src/Amazon.Cryptography/Keys/DataKey.cs
@@ -0,0 +1,10 @@
+﻿namespace Amazon.Cryptography;
+
+public readonly ref struct DataKey
+{
+    public byte[] RawKey { get; init; }
+
+    public Utf8String ProviderId { get; init; }
+
+    public Utf8String ProviderContext { get; init; }
+}
